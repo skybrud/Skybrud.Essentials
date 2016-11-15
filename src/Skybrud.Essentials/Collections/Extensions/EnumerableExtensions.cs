6c67cfd99cffbdbeed542786e1f0190240f976ac
Anders Bjerner <abjerner@skybrud.dk>
2016-11-15 23:16:56 +0100

    Added extension methods for "IEnumerable<T>"
    See #1

---
+++ src/Skybrud.Essentials/Collections/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Skybrud.Essentials.Collections.Extensions {
+
+    /// <summary>
+    /// Static class with extensions methods for instances of <code>IEnumerable</code>.
+    /// </summary>
+    public static class EnumerableExtensions {
+
+        public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> collection, Func<T, TKey> func, bool reverse) {
+            return reverse ? collection.OrderByDescending(func) : collection.OrderBy(func);
+        }
+
+        public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> collection, Func<T, TKey> func, SortOrder order) {
+            return order == SortOrder.Descending ? collection.OrderByDescending(func) : collection.OrderBy(func);
+        }
+
+    }
+
+}
---
+++ src/Skybrud.Essentials/Collections/SortOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace Skybrud.Essentials.Collections {
+
+    /// <summary>
+    /// Enum class representing the sort order of a collection.
+    /// </summary>
+    public enum SortOrder {
+
+        /// <summary>
+        /// Indicates that items are sorted in ascending order.
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// Indicates that items are sorted in descending order.
+        /// </summary>
+        Descending
+    
+    }
+
+}
