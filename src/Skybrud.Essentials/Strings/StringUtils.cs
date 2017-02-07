83b0d1e066e02217cb11de5c09963c0630b5afcf
Anders Bjerner <abjerner@skybrud.dk>
2017-02-07 19:10:15 +0100

    Improved XML documentation

--- src/Skybrud.Essentials/Strings/StringUtils.cs
+++ src/Skybrud.Essentials/Strings/StringUtils.cs
@@ -11,26 +11,26 @@ namespace Skybrud.Essentials.Strings {
     public static class StringUtils {
 
         /// <summary>
-        /// Parses the specified <code>str</code> into an instance of <see cref="System.Boolean"/>. The string is
+        /// Parses the specified <paramref name="str"/> into an instance of <see cref="System.Boolean"/>. The string is
         /// considered <code>true</code> if it matches either <code>1</code>, <code>t</code> or <code>true</code>
         /// (case insensitive).
         /// </summary>
         /// <param name="str">The string to be parsed.</param>
-        /// <returns>Returns <code>true</code> if <code>str</code> matches either <code>true</code>, <code>1</code>
-        /// or <code>t</code> (case insensitive).</returns>
+        /// <returns><code>true</code> if <paramref name="str"/> matches either <code>true</code>, <code>1</code> or
+        /// <code>t</code> (case insensitive).</returns>
         public static bool ParseBoolean(string str) {
             str = (str ?? "").ToLower();
             return str == "true" || str == "1" || str == "t";
         }
 
         /// <summary>
-        /// Parses the specified <code>value</code> into an instance of <see cref="System.Boolean"/>. The value is
-        /// considered <code>true</code> if it matches either <code>1</code>, <code>t</code> or <code>true</code>
+        /// Parses the specified <paramref name="value"/> into an instance of <see cref="System.Boolean"/>. The value
+        /// is considered <code>true</code> if it matches either <code>1</code>, <code>t</code> or <code>true</code>
         /// (case insensitive).
         /// </summary>
         /// <param name="value">The value to be parsed.</param>
-        /// <returns>Returns <code>true</code> if <code>value</code> matches either <code>true</code>, <code>1</code>
-        /// or <code>t</code> (case insensitive).</returns>
+        /// <returns><code>true</code> if <paramref name="value"/> matches either <code>true</code>, <code>1</code> or
+        /// <code>t</code> (case insensitive).</returns>
         public static bool ParseBoolean(object value) {
             return ParseBoolean(value + "");
         }
@@ -39,7 +39,7 @@ namespace Skybrud.Essentials.Strings {
         /// Converts a comma separated string into an array of integers.
         /// </summary>
         /// <param name="str">The comma separated string to be converted.</param>
-        /// <returns>Returns an array of <see cref="Int32"/>.</returns>
+        /// <returns>An array of <see cref="Int32"/>.</returns>
         public static int[] CsvToInt(string str) {
             return (
                 from piece in (str ?? "").Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
@@ -49,10 +49,10 @@ namespace Skybrud.Essentials.Strings {
         }
 
         /// <summary>
-        /// Converts the specified <code>str</code> to camel case (also referred to as lower camel casing).
+        /// Converts the specified <paramref name="str"/> to camel case (also referred to as lower camel casing).
         /// </summary>
         /// <param name="str">The string to be converted.</param>
-        /// <returns>Returns the camel cased string.</returns>
+        /// <returns>The camel cased string.</returns>
         public static string ToCamelCase(string str) {
 
             // Convert the string to lowercase initially for better results (eg. if the string is already camel cased)
@@ -76,10 +76,10 @@ namespace Skybrud.Essentials.Strings {
         }
 
         /// <summary>
-        /// Converts the specified <code>str</code> to Pascal case (also referred to as upper camel casing).
+        /// Converts the specified <paramref name="str"/> to Pascal case (also referred to as upper camel casing).
         /// </summary>
         /// <param name="str">The string to be converted.</param>
-        /// <returns>Returns the Pascal cased string.</returns>
+        /// <returns>The Pascal cased string.</returns>
         public static string ToPascalCase(string str) {
 
             // Convert the string to lowercase initially for better results (eg. if the string is already camel cased)
@@ -97,16 +97,16 @@ namespace Skybrud.Essentials.Strings {
         /// Converts the name of the specified enum <code>value</code> to a Pascal cased string.
         /// </summary>
         /// <param name="value">The enum value to be converted.</param>
-        /// <returns>Returns the Pascal cased string.</returns>
+        /// <returns>The Pascal cased string.</returns>
         public static string ToPascalCase(Enum value) {
             return ToPascalCase(value.ToString());
         }
 
         /// <summary>
-        /// Converts the specified <code>str</code> to a lower case string with words separated by underscores.
+        /// Converts the specified <paramref name="str"/> to a lower case string with words separated by underscores.
         /// </summary>
         /// <param name="str">The string to be converted.</param>
-        /// <returns>Returns the converted string.</returns>
+        /// <returns>The converted string.</returns>
         public static string ToUnderscore(string str) {
 
             // Replace invalid characters
@@ -130,8 +130,8 @@ namespace Skybrud.Essentials.Strings {
         }
 
         /// <summary>
-        /// Uppercases the first character of a the specified <code>str</code>. If <code>str</code> is either
-        /// <code>null</code> or empty, an empty string will be returned instead.
+        /// Uppercases the first character of a the specified <paramref name="str"/>. If <paramref name="str"/> is
+        /// either <code>null</code> or empty, an empty string will be returned instead.
         /// </summary>
         /// <param name="str">The string which first character should be uppercased.</param>
         /// <returns>The input string with the first character has been uppercased.</returns>
@@ -140,19 +140,19 @@ namespace Skybrud.Essentials.Strings {
         }
 
         /// <summary>
-        /// Encodes a URL string.
+        /// URL encodes the specified <paramref name="str"/>.
         /// </summary>
         /// <param name="str">The string to be encoded.</param>
-        /// <returns>Returns the encoded string.</returns>
+        /// <returns>The URL encoded string.</returns>
         public static string UrlEncode(string str) {
             return HttpUtility.UrlEncode(str);
         }
 
         /// <summary>
-        /// Decodes a URL string.
+        /// URL decodes a URL string.
         /// </summary>
         /// <param name="str">The string to be decoded.</param>
-        /// <returns>Returns the decoded string.</returns>
+        /// <returns>The URL decoded string.</returns>
         public static string UrlDecode(string str) {
             return HttpUtility.UrlDecode(str);
         }
