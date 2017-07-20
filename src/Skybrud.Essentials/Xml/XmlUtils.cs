c866b9764167827611e7c63196872d580305ed2f
Anders Bjerner <abjerner@skybrud.dk>
2017-07-20 15:54:32 +0200

    Improved XML documentation

--- src/Skybrud.Essentials/Xml/XmlUtils.cs
+++ src/Skybrud.Essentials/Xml/XmlUtils.cs
@@ -10,71 +10,73 @@ namespace Skybrud.Essentials.Xml {
     public static class XmlUtils {
 
         /// <summary>
-        /// Parses the specified <code>xml</code> into an instance of <see cref="XElement"/>.
+        /// Parses the specified <paramref name="xml"/> into an instance of <see cref="XElement"/>.
         /// </summary>
         /// <param name="xml">The XML to be parsed.</param>
-        /// <returns>Returns an instance of <see cref="XElement"/>.</returns>
+        /// <returns>An instance of <see cref="XElement"/>.</returns>
         public static XElement ParseXmlElement(string xml) {
             return XElement.Parse(xml);
         }
 
         /// <summary>
-        /// Parses the specified <code>xml</code> into an instance of <see cref="XElement"/>, which is then converted
-        /// into an instance of <code>T</code> using the specified <code>callback</code> function.
+        /// Parses the specified <paramref name="xml"/> into an instance of <see cref="XElement"/>, which is then
+        /// converted into an instance of <typeparamref name="T"/> using the specified <paramref name="callback"/>
+        /// function.
         /// </summary>
         /// <typeparam name="T">The type of the instance to be returned.</typeparam>
         /// <param name="xml">The XML to be parsed.</param>
         /// <param name="callback">The callback function used for converted the parsed <see cref="XElement"/>.</param>
-        /// <returns>Returns an instance of <code>T</code> representing the specified <code>xml</code>.</returns>
+        /// <returns>An instance of <typeparamref name="T"/> representing the specified <paramref name="xml"/>.</returns>
         public static T ParseXmlElement<T>(string xml, Func<XElement, T> callback) {
             return callback(XElement.Parse(xml));
         }
 
         /// <summary>
-        /// Parses the specified <code>xml</code> into an instance of <see cref="XDocument"/>.
+        /// Parses the specified <paramref name="xml"/> into an instance of <see cref="XDocument"/>.
         /// </summary>
         /// <param name="xml">The XML to be parsed.</param>
-        /// <returns>Returns an instance of <see cref="XDocument"/>.</returns>
+        /// <returns>An instance of <see cref="XDocument"/>.</returns>
         public static XDocument ParseXmlDocument(string xml) {
             return XDocument.Parse(xml);
         }
 
         /// <summary>
-        /// Parses the specified <code>xml</code> into an instance of <see cref="XDocument"/>, which is then converted
-        /// into an instance of <code>T</code> using the specified <code>callback</code> function.
+        /// Parses the specified <paramref name="xml"/> into an instance of <see cref="XDocument"/>, which is then
+        /// converted into an instance of <typeparamref name="T"/> using the specified <paramref name="callback"/>
+        /// function.
         /// </summary>
         /// <typeparam name="T">The type of the instance to be returned.</typeparam>
         /// <param name="xml">The XML to be parsed.</param>
         /// <param name="callback">The callback function used for converted the parsed <see cref="XDocument"/>.</param>
-        /// <returns>Returns an instance of <code>T</code> representing the specified <code>xml</code>.</returns>
+        /// <returns>An instance of <typeparamref name="T"/> representing the specified <paramref name="xml"/>.</returns>
         public static T ParseXmlDocument<T>(string xml, Func<XDocument, T> callback) {
             return callback(XDocument.Parse(xml));
         }
 
         /// <summary>
-        /// Loads the XML document at specified <code>path</code> and returns it as an instance of
+        /// Loads the XML document at specified <paramref name="path"/> and returns it as an instance of
         /// <see cref="XElement"/>.
         /// </summary>
         /// <param name="path">The path to the XML document.</param>
-        /// <returns>Returns an instance of <see cref="XElement"/>.</returns>
+        /// <returns>An instance of <see cref="XElement"/>.</returns>
         public static XElement LoadXmlElement(string path) {
             return XElement.Load(path);
         }
 
         /// <summary>
-        /// Loads the XML document at specified <code>path</code>, which is then converted into an instance of
-        /// <code>T</code> using the specified <code>callback</code> function.
+        /// Loads the XML document at the specified <paramref name="path"/>, which is then converted into an instance
+        /// of <typeparamref name="T"/> using the specified <paramref name="callback"/> function.
         /// </summary>
         /// <typeparam name="T">The type of the instance to be returned.</typeparam>
         /// <param name="path">The path to the XML document.</param>
         /// <param name="callback">The callback function used for converting the loaded <see cref="XElement"/>.</param>
-        /// <returns>Returns an instance of <code>T</code> representing the specified <code>xml</code>.</returns>
+        /// <returns>An instance of <typeparamref name="T"/> representing the XML element.</returns>
         public static T LoadXmlElement<T>(string path, Func<XElement, T> callback) {
             return callback(XElement.Load(path));
         }
 
         /// <summary>
-        /// Loads the XML document at specified <code>path</code> and returns it as an instance of
+        /// Loads the XML document at the specified <paramref name="path"/> and returns it as an instance of
         /// <see cref="XDocument"/>.
         /// </summary>
         /// <param name="path">The path to the XML document.</param>
@@ -84,13 +86,13 @@ namespace Skybrud.Essentials.Xml {
         }
 
         /// <summary>
-        /// Loads the XML document at specified <code>path</code>, which is then converted into an instance of
-        /// <code>T</code> using the specified <code>callback</code> function.
+        /// Loads the XML document at the specified <paramref name="path"/>, which is then converted into an instance
+        /// of <typeparamref name="T"/> using the specified <paramref name="callback"/> function.
         /// </summary>
         /// <typeparam name="T">The type of the instance to be returned.</typeparam>
         /// <param name="path">The path to the XML document.</param>
         /// <param name="callback">The callback function used for converting the loaded <see cref="XDocument"/>.</param>
-        /// <returns>Returns an instance of <code>T</code> representing the XML document.</returns>
+        /// <returns>Returns an instance of <typeparamref name="T"/> representing the XML document.</returns>
         public static T LoadXmlDocument<T>(string path, Func<XDocument, T> callback) {
             return callback(XDocument.Load(path));
         }
