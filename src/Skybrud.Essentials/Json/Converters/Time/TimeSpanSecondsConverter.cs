9d4294d4b79c6ee4f2971bece359d0c566e051af
Anders Bjerner <abjerner@skybrud.dk>
2017-12-22 14:21:12 +0100

    Introduced new "TimeSpanSecondsConverter" JSON converter

---
+++ src/Skybrud.Essentials/Json/Converters/Time/TimeSpanSecondsConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace Skybrud.Essentials.Json.Converters.Time {
+
+    /// <summary>
+    /// JSON converter for seriqalizing and deserializating between instances of <see cref="TimeSpan"/> and the total
+    /// amount of seconds represented by the time span.
+    /// </summary>
+    public class TimeSpanSecondsConverter : JsonConverter {
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            if (value is TimeSpan) {
+                TimeSpan ts = (TimeSpan) value;
+                writer.WriteRawValue(ts.TotalSeconds.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+
+            if (objectType != typeof(TimeSpan)) throw new JsonSerializationException("Object type " + objectType + " is not supported");
+
+            switch (reader.TokenType) {
+
+                case JsonToken.Null:
+                    return default(TimeSpan);
+
+                case JsonToken.Integer:
+                case JsonToken.Float:
+                    return TimeSpan.FromSeconds((double) reader.Value);
+
+                default:
+                    throw new JsonSerializationException("Unexpected token type: " + reader.TokenType);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns><code>true</code> if this instance can convert the specified object type; otherwise,
+        /// <code>false</code>.</returns>
+        public override bool CanConvert(Type objectType) {
+            return objectType == typeof(TimeSpan);
+        }
+
+    }
+
+}
---
+++ src/UnitTestProject1/Json/Converters/TimeSpanSecondsConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Skybrud.Essentials.Json.Converters.Time;
+
+namespace UnitTestProject1.Json.Converters {
+
+    [TestClass]
+    public class TimeSpanSecondsConverterTests {
+
+        [TestMethod]
+        public void Serialize() {
+
+            Sample sample1 = new Sample { Duration = TimeSpan.FromSeconds(42) };
+            Sample sample2 = new Sample { Duration = TimeSpan.FromSeconds(3.14) };
+
+            Console.WriteLine(sample2.Duration.TotalSeconds);
+            Console.WriteLine();
+
+            Assert.AreEqual("{\"duration\":42}", JsonConvert.SerializeObject(sample1));
+            Assert.AreEqual("{\"duration\":3.14}", JsonConvert.SerializeObject(sample2));
+
+        }
+
+        [TestMethod]
+        public void Deserialize() {
+            
+            Sample sample1 = new Sample { Duration = TimeSpan.FromSeconds(42) };
+            Sample sample2 = new Sample { Duration = TimeSpan.FromSeconds(3.14) };
+
+            Sample parsed1 = JsonConvert.DeserializeObject<Sample>("{\"duration\":42}");
+            Assert.AreEqual(sample1.Duration, parsed1.Duration);
+            Assert.AreEqual(sample1.Duration, parsed1.Duration);
+
+            Sample parsed2 = JsonConvert.DeserializeObject<Sample>("{\"duration\":3.14}");
+            Assert.AreEqual(sample2.Duration, parsed2.Duration);
+            Assert.AreEqual(sample2.Duration, parsed2.Duration);
+
+        }
+
+        class Sample {
+
+            [JsonProperty("duration")]
+            [JsonConverter(typeof(TimeSpanSecondsConverter))]
+            public TimeSpan Duration { get; set; }
+
+        }
+
+    }
+
+}
