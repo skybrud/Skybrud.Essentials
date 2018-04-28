f394f2401cf5786778850ed8611952489ae9b3ba
Anders Bjerner <abjerner@skybrud.dk>
2018-04-28 17:07:38 +0200

    The "ParseBoolean" will now also return true if input string matches "on"

--- src/Skybrud.Essentials/Strings/StringUtils.cs
+++ src/Skybrud.Essentials/Strings/StringUtils.cs
@@ -13,23 +13,21 @@ namespace Skybrud.Essentials.Strings {
 
         /// <summary>
         /// Parses the specified <paramref name="str"/> into an instance of <see cref="Boolean"/>. The string is
-        /// considered <c>true</c> if it matches either <c>1</c>, <typeparamref name="T"/> or <c>true</c> (case insensitive).
+        /// considered <c>true</c> if it matches either <c>true</c>, <c>1</c>, <c>t</c> or <c>on</c> (case insensitive).
         /// </summary>
         /// <param name="str">The string to be parsed.</param>
-        /// <returns><c>true</c> if <paramref name="str"/> matches either <c>true</c>, <c>1</c> or
-        /// <typeparamref name="T"/> (case insensitive).</returns>
+        /// <returns><c>true</c> if <paramref name="str"/> matches either <c>true</c>, <c>1</c>, <c>t</c> or <c>on</c> (case insensitive).</returns>
         public static bool ParseBoolean(string str) {
             str = (str ?? "").ToLower();
-            return str == "true" || str == "1" || str == "t";
+            return str == "true" || str == "1" || str == "t" || str == "on";
         }
 
         /// <summary>
         /// Parses the specified <paramref name="value"/> into an instance of <see cref="Boolean"/>. The value
-        /// is considered <c>true</c> if it matches either <c>1</c>, <typeparamref name="T"/> or <c>true</c> (case insensitive).
+        /// is considered <c>true</c> if it matches either <c>true</c>, <c>1</c>, <c>t</c> or <c>on</c> (case insensitive).
         /// </summary>
         /// <param name="value">The value to be parsed.</param>
-        /// <returns><c>true</c> if <paramref name="value"/> matches either <c>true</c>, <c>1</c> or
-        /// <typeparamref name="T"/> (case insensitive).</returns>
+        /// <returns><c>true</c> if <paramref name="value"/> matches either <c>true</c>, <c>1</c>, <c>t</c> or <c>on</c> (case insensitive).</returns>
         public static bool ParseBoolean(object value) {
             return ParseBoolean(value + "");
         }
