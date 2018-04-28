db16d89a24007eacb418293a887bd97929ec471e
Anders Bjerner <abjerner@skybrud.dk>
2018-04-28 15:13:36 +0200

    Methods related to casing should have their own file

---
+++ src/Skybrud.Essentials/Strings/Extensions/StringExtensions.Casing.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Strings.Extensions {
+
+    public static partial class StringExtensions {
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to camel case (also referred to as lower camel casing).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToCamelCase(this string str) {
+            return StringUtils.ToCamelCase(str);
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a camel cased string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToCamelCase(this Enum value) {
+            return StringUtils.ToCamelCase(value);
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to Pascal case (also referred to as upper camel casing).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The Pascal cased string.</returns>
+        public static string ToPascalCase(this string str) {
+            return StringUtils.ToPascalCase(str);
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a Pascal cased string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The Pascal cased string.</returns>
+        public static string ToPascalCase(this Enum value) {
+            return StringUtils.ToPascalCase(value);
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a kebab cased string (lower case words separated by hyphens).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The kebab cased string.</returns>
+        public static string ToKebabCase(this string str) {
+            return StringUtils.ToKebabCase(str);
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a kebab cased string (lower case words separated by hyphens).
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToKebabCase(this Enum value) {
+            return StringUtils.ToKebabCase(value);
+        }
+        
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a train cased string (upper case words separated by hyphens).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The train cased string.</returns>
+        public static string ToTrainCase(this string str) {
+            return StringUtils.ToTrainCase(str);
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a train cased string (upper case words separated by hyphens).
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToTrainCase(this Enum value) {
+            return StringUtils.ToTrainCase(value);
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a lower case string with words separated by underscores.
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The converted string.</returns>
+        public static string ToUnderscore(this string str) {
+            return StringUtils.ToTrainCase(str);
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to a lower case string with words separated by
+        /// underscores.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The converted string.</returns>
+        public static string ToUnderscore(this Enum value) {
+            return StringUtils.ToTrainCase(value);
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to a lower case string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The lower case version of <paramref name="value"/>.</returns>
+        public static string ToLower(this Enum value) {
+            return value.ToString().ToLower();
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to an upper case string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The upper case version of <paramref name="value"/>.</returns>
+        public static string ToUpper(this Enum value) {
+            return value.ToString().ToUpper();
+        }
+
+        /// <summary>
+        /// Uppercases the first character of a the specified <paramref name="str"/>. If <paramref name="str"/> is
+        /// either <c>null</c> or empty, an empty string will be returned instead.
+        /// </summary>
+        /// <param name="str">The string which first character should be uppercased.</param>
+        /// <returns>The input string with the first character has been uppercased.</returns>
+        public static string FirstCharToUpper(this string str) {
+            return String.IsNullOrEmpty(str) ? "" : String.Concat(str.Substring(0, 1).ToUpper(), str.Substring(1));
+        }
+
+    }
+
+}
---
+++ src/Skybrud.Essentials/Strings/StringUtils.Casing.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Skybrud.Essentials.Strings {
+
+    public static partial class StringUtils {
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to camel case (also referred to as lower camel casing).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToCamelCase(string str) {
+
+            // Convert the string to lowercase initially for better results (eg. if the string is already camel cased)
+            str = ToUnderscore(str);
+            
+            // Split the string by space or underscore
+            string[] pieces = str.Split(new[] { ' ', '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Join the pieces again and uppercase the first character of each piece but the first
+            return String.Join("", pieces.Select((t, i) => i == 0 ? t : FirstCharToUpper(t)));
+
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a camel cased string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToCamelCase(Enum value) {
+            return ToCamelCase(value.ToString());
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to Pascal case (also referred to as upper camel casing).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The Pascal cased string.</returns>
+        public static string ToPascalCase(string str) {
+
+            // Convert the string to lowercase initially for better results (eg. if the string is already camel cased)
+            str = ToUnderscore(str);
+
+            // Split the string by space or underscore
+            string[] pieces = str.Split(new[] { ' ', '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Join the pieces again and uppercase the first character of each piece
+            return String.Join("", from piece in pieces select FirstCharToUpper(piece));
+
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a Pascal cased string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The Pascal cased string.</returns>
+        public static string ToPascalCase(Enum value) {
+            return ToPascalCase(value.ToString());
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a kebab cased string (lower case words separated by hyphens).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The kebab cased string.</returns>
+        public static string ToKebabCase(string str) {
+
+            // Replace invalid characters
+            str = Regex.Replace(str ?? "", "[\\W_]+", " ").Trim();
+
+            // Replace multiple whitespaces
+            str = Regex.Replace(str, "[ ]{2,}", " ");
+
+            // Convert to lower case (with upper case letters prefixed with hyphens)
+            return Regex.Replace(str, @"(\p{Ll})(\p{Lu})", "$1-$2").Replace(" ", "-").Replace("--", "-").ToLower();
+
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a kebab cased string (lower case words separated by hyphens).
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToKebabCase(Enum value) {
+            return ToKebabCase(value.ToString());
+        }
+        
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a train cased string (upper case words separated by hyphens).
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The train cased string.</returns>
+        public static string ToTrainCase(string str) {
+
+            // Replace invalid characters
+            str = Regex.Replace(str ?? "", "[\\W_]+", " ").Trim();
+
+            // Replace multiple whitespaces
+            str = Regex.Replace(str, "[ ]{2,}", " ");
+
+            // Convert to upper case (with upper case letters prefixed with hyphens)
+            return Regex.Replace(str, @"(\p{Ll})(\p{Lu})", "$1-$2").Replace(" ", "-").Replace("--", "-").ToUpper();
+
+        }
+
+        /// <summary>
+        /// Converts the name of the specified enum <paramref name="value"/> to a train cased string (upper case words separated by hyphens).
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The camel cased string.</returns>
+        public static string ToTrainCase(Enum value) {
+            return ToTrainCase(value.ToString());
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="str"/> to a lower case string with words separated by underscores.
+        /// </summary>
+        /// <param name="str">The string to be converted.</param>
+        /// <returns>The converted string.</returns>
+        public static string ToUnderscore(string str) {
+
+            // Replace invalid characters
+            str = Regex.Replace(str ?? "", "[\\W_]+", " ").Trim();
+
+            // Replace multiple whitespaces
+            str = Regex.Replace(str, "[ ]{2,}", " ");
+
+            // Convert to lowercase (with uppercase letters prefixed with underscore)
+            return Regex.Replace(str, @"(\p{Ll})(\p{Lu})", "$1_$2").Replace(" ", "_").Replace("__", "_").ToLower();
+
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to a lower case string with words separated by
+        /// underscores.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The converted string.</returns>
+        public static string ToUnderscore(Enum value) {
+            return ToUnderscore(value.ToString());
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to a lower case string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The lower case version of <paramref name="value"/>.</returns>
+        public static string ToLower(Enum value) {
+            return value.ToString().ToLower();
+        }
+
+        /// <summary>
+        /// Converts the specified enum <paramref name="value"/> to an upper case string.
+        /// </summary>
+        /// <param name="value">The enum value to be converted.</param>
+        /// <returns>The upper case version of <paramref name="value"/>.</returns>
+        public static string ToUpper(Enum value) {
+            return value.ToString().ToUpper();
+        }
+
+        /// <summary>
+        /// Uppercases the first character of a the specified <paramref name="str"/>. If <paramref name="str"/> is
+        /// either <c>null</c> or empty, an empty string will be returned instead.
+        /// </summary>
+        /// <param name="str">The string which first character should be uppercased.</param>
+        /// <returns>The input string with the first character has been uppercased.</returns>
+        public static string FirstCharToUpper(string str) {
+            return String.IsNullOrEmpty(str) ? "" : String.Concat(str.Substring(0, 1).ToUpper(), str.Substring(1));
+        }
+
+    }
+
+}
