cb4ab55a917db1a433b1ee8faad0be42a891f229
Anders Bjerner <abjerner@skybrud.dk>
2019-09-12 19:49:00 +0200

    UnixTimeConverter should inherit from TimeConverter and no longer be obsolete

--- src/Skybrud.Essentials/Json/Converters/Time/UnixTimeConverter.cs
+++ src/Skybrud.Essentials/Json/Converters/Time/UnixTimeConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
 using Skybrud.Essentials.Time;
 
 namespace Skybrud.Essentials.Json.Converters.Time {
@@ -7,74 +6,12 @@ namespace Skybrud.Essentials.Json.Converters.Time {
     /// <summary>
     /// Converts an instance of either <see cref="EssentialsDateTime"/> or <see cref="DateTime"/> to and from a Unix timestamp.
     /// </summary>
-    [Obsolete("Use TimeConverter instead.")]
-    public class UnixTimeConverter : JsonConverter {
+    public class UnixTimeConverter : TimeConverter {
 
         /// <summary>
-        /// Writes the JSON representation of the object.
+        /// Initializes a new converter with default options.
         /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
-
-            if (value is EssentialsDateTime) {
-                EssentialsDateTime dt = (EssentialsDateTime) value;
-                writer.WriteValue(dt.UnixTimestamp);
-            }
-
-            if (value is DateTime) {
-                DateTime dt = (DateTime) value;
-                writer.WriteValue(TimeUtils.GetUnixTimeFromDateTime(dt));
-            }
-
-        }
-
-        /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <param name="existingValue">The existing value of object being read.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        /// <returns>The object value.</returns>
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
-
-            double timestamp;
-
-            switch (reader.TokenType) {
-
-                case JsonToken.Null:
-                    return objectType == typeof(DateTime) ? (object) default(DateTime) : null;
-
-                case JsonToken.Integer:
-                    timestamp = (long) reader.Value; break;
-
-                case JsonToken.Float:
-                    timestamp = (double) reader.Value; break;
-
-                case JsonToken.String:
-                    if (!Double.TryParse(reader.Value + "", out timestamp)) throw new JsonSerializationException("String value doesn't match a Unix timestamp: " + reader.Value); break;
-
-                default:
-                    throw new JsonSerializationException("Unexpected token type: " + reader.TokenType);
-
-            }
-
-            if (objectType == typeof(EssentialsDateTime)) return EssentialsDateTime.FromUnixTimestamp(timestamp);
-            if (objectType == typeof(DateTime)) return TimeUtils.GetDateTimeFromUnixTime(timestamp);
-            throw new JsonSerializationException("Object type " + objectType + " is not supported");
-
-        }
-
-        /// <summary>
-        /// Determines whether this instance can convert the specified object type.
-        /// </summary>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns><c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.</returns>
-        public override bool CanConvert(Type objectType) {
-            return objectType == typeof(EssentialsDateTime) || objectType == typeof(DateTime);
-        }
+        public UnixTimeConverter() : base(TimeFormat.UnixTime) { }
 
     }
 
