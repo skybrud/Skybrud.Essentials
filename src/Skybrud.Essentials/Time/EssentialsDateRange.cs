d6f05a71561119034af303fed4686f76cc244a2d
Anders Bjerner <abjerner@skybrud.dk>
2019-10-20 15:11:00 +0200

    Introduced new "EssentialsDateRange" class

---
+++ src/Skybrud.Essentials/Time/EssentialsDateRange.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Skybrud.Essentials.Time {
+
+    /// <summary>
+    /// Class representing a range of one or more <see cref="EssentialsDateRange"/>.
+    /// </summary>
+    public class EssentialsDateRange : IEnumerable<EssentialsDate> {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the <see cref="EssentialsDate"/> at the start of the range.
+        /// </summary>
+        public EssentialsDate Start { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="EssentialsDate"/> at the end of the range.
+        /// </summary>
+        public EssentialsDate End { get; set; }
+
+        /// <summary>
+        /// Gets an array of <see cref="EssentialsDate"/> within the range.
+        /// </summary>
+        public EssentialsDate[] Days { get; set; }
+
+        /// <summary>
+        /// Indicates whether the range is in reverse order (if <see cref="End"/> is before <see cref="Start"/>).
+        /// </summary>
+        public bool IsReverse { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new range from the specified <paramref name="start"/> and <paramref name="end"/>.
+        /// </summary>
+        /// <param name="start">The <see cref="EssentialsDate"/> at the start of the range.</param>
+        /// <param name="end">The <see cref="EssentialsDate"/> at the end of the range.</param>
+        public EssentialsDateRange(EssentialsDate start, EssentialsDate end) {
+
+            Start = start;
+            End = end;
+
+            List<EssentialsDate> temp = new List<EssentialsDate>();
+
+            if (end < start) {
+                IsReverse = true;
+                for (EssentialsDate date = start; date.YearAndDay >= end.YearAndDay; date = date.AddDays(-1)) {
+                    temp.Add(date);
+                }
+            } else {
+                for (EssentialsDate date = start; date.YearAndDay <= end.YearAndDay; date = date.AddDays(+1)) {
+                    temp.Add(date);
+                }
+            }
+
+            Days = temp.ToArray();
+
+        }
+
+        #endregion
+
+        #region Member methods
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="EssentialsDate"/> of the range.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the <see cref="EssentialsDate"/> of the range.</returns>
+        public IEnumerator<EssentialsDate> GetEnumerator() {
+            return ((IEnumerable<EssentialsDate>) Days).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/UnitTestProject1/Time/EssentialsDateRangeTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Time;
+// ReSharper disable ExpressionIsAlwaysNull
+
+#pragma warning disable 618
+
+namespace UnitTestProject1.Time {
+
+    [TestClass]
+    public class EssentialsDateRangeTests {
+
+        [TestMethod]
+        public void Default() {
+
+            EssentialsDate start = new EssentialsDate(2019, 10, 14);
+            EssentialsDate end = new EssentialsDate(2019, 10, 20);
+
+            var range = new EssentialsDateRange(start, end);
+
+            Assert.AreEqual(false, range.IsReverse, "# reverse");
+
+            Assert.AreEqual(7, range.Days.Length, "# length");
+
+            Assert.AreEqual("2019-10-14", range.Days[0].Iso8601, "# index 0");
+            Assert.AreEqual("2019-10-15", range.Days[1].Iso8601, "# index 1");
+            Assert.AreEqual("2019-10-16", range.Days[2].Iso8601, "# index 2");
+            Assert.AreEqual("2019-10-17", range.Days[3].Iso8601, "# index 3");
+            Assert.AreEqual("2019-10-18", range.Days[4].Iso8601, "# index 4");
+            Assert.AreEqual("2019-10-19", range.Days[5].Iso8601, "# index 5");
+            Assert.AreEqual("2019-10-20", range.Days[6].Iso8601, "# index 6");
+
+        }
+
+        [TestMethod]
+        public void Reverse() {
+
+            EssentialsDate start = new EssentialsDate(2019, 10, 20);
+            EssentialsDate end = new EssentialsDate(2019, 10, 14);
+
+            var range = new EssentialsDateRange(start, end);
+
+            Assert.AreEqual(true, range.IsReverse, "# reverse");
+
+            Assert.AreEqual(7, range.Days.Length, "# length");
+
+            Assert.AreEqual("2019-10-20", range.Days[0].Iso8601, "# index 0");
+            Assert.AreEqual("2019-10-19", range.Days[1].Iso8601, "# index 1");
+            Assert.AreEqual("2019-10-18", range.Days[2].Iso8601, "# index 2");
+            Assert.AreEqual("2019-10-17", range.Days[3].Iso8601, "# index 3");
+            Assert.AreEqual("2019-10-16", range.Days[4].Iso8601, "# index 4");
+            Assert.AreEqual("2019-10-15", range.Days[5].Iso8601, "# index 5");
+            Assert.AreEqual("2019-10-14", range.Days[6].Iso8601, "# index 6");
+
+        }
+
+    }
+
+}
+
+#pragma warning restore 618
