435dd7e7b90e871ea068846b98b7c1ed6258b7f3
Anders Bjerner <abjerner@skybrud.dk>
2019-10-20 20:25:29 +0200

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,4 @@
 @echo off
 cd src/Skybrud.Essentials
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
 cd ../../
-grunt
