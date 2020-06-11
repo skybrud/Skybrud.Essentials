2bd1ed1442636bf825508dcde12c6994df268a91
Anders Bjerner <abjerner@skybrud.dk>
2020-06-11 20:12:24 +0200

    Introduced OrderBy overlay

--- src/Skybrud.Essentials/Collections/Extensions/EnumerableExtensions.cs
+++ src/Skybrud.Essentials/Collections/Extensions/EnumerableExtensions.cs
@@ -37,6 +37,37 @@ namespace Skybrud.Essentials.Collections.Extensions {
             return order == SortOrder.Descending ? collection.OrderByDescending(func) : collection.OrderBy(func);
         }
 
+        /// <summary>
+        /// Sorts the elements of <paramref name="source"/> in descending order if <paramref name="reverse"/> is
+        /// <c>true</c>, otherwise in ascending order.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the collection.</typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source">The collection to be sorted.</param>
+        /// <param name="func">The callback function used for sorting the items.</param>
+        /// <returns>An instanced of <see cref="IOrderedEnumerable{T}"/>.</returns>
+        /// <param name="comparer">An <see cref="IComparer{T}"/> to compare the keys.</param>
+        /// <param name="reverse">Whether <paramref name="source"/> should be sorted in descending order.</param>
+        /// <returns>An instanced of <see cref="IOrderedEnumerable{T}"/>.</returns>
+        public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> func, IComparer<TKey> comparer, bool reverse) {
+            return reverse ? source.OrderByDescending(func, comparer) : source.OrderBy(func, comparer);
+        }
+
+        /// <summary>
+        /// Orders <paramref name="source"/> in descending order if <paramref name="order"/> is
+        /// <see cref="SortOrder.Descending"/>, otherwise in ascending order.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/>.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="keySelector">A function to extract a key from an element.</param>
+        /// <param name="comparer">An <see cref="IComparer{T}"/> to compare the keys.</param>
+        /// <param name="order">The order by which the collection should be sorted.</param>
+        /// <returns>An instanced of <see cref="IOrderedEnumerable{T}"/>.</returns>
+        public static IOrderedEnumerable<TSource> OrderBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer, SortOrder order) {
+            return order == SortOrder.Descending ? source.OrderByDescending(keySelector, comparer) : source.OrderBy(keySelector, comparer);
+        }
+
     }
 
 }
---
+++ src/UnitTestProject1/Collections/CollectionTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Collections;
+using Skybrud.Essentials.Collections.Extensions;
+
+namespace UnitTestProject1.Collections {
+
+    [TestClass]
+    public class CollectionTests {
+        
+        [TestMethod]
+        public void OrderByComparerBoolean() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.InvariantCulture, false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, false);
+
+            Assert.AreEqual("aand,and", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByComparerEnum() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.InvariantCulture, false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, SortOrder.Ascending);
+
+            Assert.AreEqual("aand,and", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByComparerDanishBoolean() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.GetCultureInfo("da-DK"), false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, false);
+
+            Assert.AreEqual("and,aand", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByComparerDanishEnum() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.GetCultureInfo("da-DK"), false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, SortOrder.Ascending);
+
+            Assert.AreEqual("and,aand", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByDescendingComparerBoolean() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.InvariantCulture, false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, true);
+
+            Assert.AreEqual("and,aand", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByDescendingComparerEnum() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.InvariantCulture, false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, SortOrder.Descending);
+
+            Assert.AreEqual("and,aand", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByDescendingComparerDanishBoolean() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.GetCultureInfo("da-DK"), false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, true);
+
+            Assert.AreEqual("aand,and", string.Join(",", result));
+
+        }
+        
+        [TestMethod]
+        public void OrderByDescendingComparerDanishEnum() {
+
+            StringComparer comparer = StringComparer.Create(CultureInfo.GetCultureInfo("da-DK"), false);
+
+            string[] samples = {
+                "and", "aand"
+            };
+
+            IOrderedEnumerable<string> result = samples.OrderBy(x => x, comparer, SortOrder.Descending);
+
+            Assert.AreEqual("aand,and", string.Join(",", result));
+
+        }
+
+    }
+
+}
