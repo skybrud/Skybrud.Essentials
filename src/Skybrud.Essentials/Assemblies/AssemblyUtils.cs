e31ab32f5b506475818887060de946ec9aeafcae
Anders Bjerner <abjerner@skybrud.dk>
2020-06-27 21:58:23 +0200

    Introduced new ReflectionUtils class

--- src/Skybrud.Essentials/Assemblies/AssemblyUtils.cs
+++ src/Skybrud.Essentials/Assemblies/AssemblyUtils.cs
@@ -1,54 +1,56 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using Skybrud.Essentials.Reflection;
 
 namespace Skybrud.Essentials.Assemblies {
-    
+
     /// <summary>
-    /// Static utility class for working with assemblies and the <see cref="Assembly"/> class in particular.
+    /// Static utility class for working with assemblies and the <see cref="Assembly"/> class in particular. Replaced by the <see cref="ReflectionUtils"/> class.
     /// </summary>
+    [Obsolete("Use static methods in the ReflectionUtils class instead.")]
     public static class AssemblyUtils {
 
         /// <summary>
-        /// Gets the version of the specified <paramref name="assembly"/>.
+        /// Gets the version of the specified <paramref name="assembly"/>. Alias of <see cref="ReflectionUtils.GetVersion"/>.
         /// </summary>
         /// <param name="assembly">The assembly.</param>
         /// <returns>A string representing the version of the assembly.</returns>
+        [Obsolete("Use ReflectionUtils.GetVersion() method instead.")]
         public static string GetVersion(Assembly assembly) {
-            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
-            return assembly.GetName().Version.ToString();
+            return ReflectionUtils.GetVersion(assembly);
         }
 
 #if I_CAN_HAZ_FILE_VERSION_INFO
 
         /// <summary>
-        /// Gets the file version of the specified <paramref name="assembly"/>.
+        /// Gets the file version of the specified <paramref name="assembly"/>. Alias of <see cref="ReflectionUtils.GetFileVersion"/>.
         /// </summary>
         /// <param name="assembly">The assembly.</param>
         /// <returns>A string representing the file version of the assembly.</returns>
+        [Obsolete("Use ReflectionUtils.GetFileVersion() method instead.")]
         public static string GetFileVersion(Assembly assembly) {
-            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
-            return FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+            return ReflectionUtils.GetFileVersion(assembly);
         }
 
         /// <summary>
-        /// Gets the informational version of the specified <paramref name="assembly"/>.
+        /// Gets the informational version of the specified <paramref name="assembly"/>. Alias of <see cref="ReflectionUtils.GetInformationalVersion"/>.
         /// </summary>
         /// <param name="assembly">The assembly.</param>
         /// <returns>A string representing the informational version of the assembly.</returns>
+        [Obsolete("Use ReflectionUtils.GetInformationalVersion() method instead.")]
         public static string GetInformationalVersion(Assembly assembly) {
-            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
-            return FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+            return ReflectionUtils.GetInformationalVersion(assembly);
         }
 
         /// <summary>
-        /// Gets an instance of <see cref="FileVersionInfo"/> with information about the specified <paramref name="assembly"/>.
+        /// Gets an instance of <see cref="FileVersionInfo"/> with information about the specified <paramref name="assembly"/>. Alias of <see cref="ReflectionUtils.GetFileVersionInfo"/>.
         /// </summary>
         /// <param name="assembly">The assembly.</param>
         /// <returns>An instance of <see cref="FileVersionInfo"/>.</returns>
+        [Obsolete("Use ReflectionUtils.GetFileVersionInfo() method instead.")]
         public static FileVersionInfo GetFileVersionInfo(Assembly assembly) {
-            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
-            return FileVersionInfo.GetVersionInfo(assembly.Location);
+            return ReflectionUtils.GetFileVersionInfo(assembly);
         }
 
         #endif
---
+++ src/Skybrud.Essentials/Reflection/ReflectionUtils.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Skybrud.Essentials.Reflection {
+
+    /// <summary>
+    /// Static utility class for working with reflection.
+    /// </summary>
+    public static class ReflectionUtils {
+
+        /// <summary>
+        /// Returns the version of the specified <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>A string representing the version of the assembly.</returns>
+        public static string GetVersion(Assembly assembly) {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            return assembly.GetName().Version.ToString();
+        }
+
+#if I_CAN_HAZ_FILE_VERSION_INFO
+
+        /// <summary>
+        /// Returns the file version of the specified <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>A string representing the file version of the assembly.</returns>
+        public static string GetFileVersion(Assembly assembly) {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            return FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+        }
+
+        /// <summary>
+        /// Returns the informational version of the specified <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>A string representing the informational version of the assembly.</returns>
+        public static string GetInformationalVersion(Assembly assembly) {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            return FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+        }
+
+        /// <summary>
+        /// Returns an instance of <see cref="FileVersionInfo"/> with information about the specified <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>An instance of <see cref="FileVersionInfo"/>.</returns>
+        public static FileVersionInfo GetFileVersionInfo(Assembly assembly) {
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            return FileVersionInfo.GetVersionInfo(assembly.Location);
+        }
+
+#endif
+
+        /// <summary>
+        /// Returns whether <typeparamref name="T"/> is a class type.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <returns><c>true</c> if <typeparamref name="T"/> is a class type; otherwise <c>false</c>.</returns>
+        public static bool IsClass<T>() {
+            return IsClass(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns whether the specified <paramref name="type"/> is a class type.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <returns><c>true</c> if <paramref name="type"/> is a class type; otherwise <c>false</c>.</returns>
+        public static bool IsClass(Type type) {
+            
+#if NET_FRAMEWORK
+            return type.IsClass;
+#endif
+            
+#if NET_STANDARD
+            return type.GetTypeInfo().IsClass;
+#endif
+
+        }
+
+        /// <summary>
+        /// Returns whether <typeparamref name="T"/> is an interface type.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <returns><c>true</c> if <typeparamref name="T"/> is an interface type; otherwise <c>false</c>.</returns>
+        public static bool IsInterface<T>() {
+            return IsInterface(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns whether the specified <paramref name="type"/> is an interface type.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <returns><c>true</c> if <paramref name="type"/> is an interface type; otherwise <c>false</c>.</returns>
+        public static bool IsInterface(Type type) {
+            
+#if NET_FRAMEWORK
+            return type.IsInterface;
+#endif
+            
+#if NET_STANDARD
+            return type.GetTypeInfo().IsInterface;
+#endif
+
+        }
+
+        /// <summary>
+        /// Returns whether <typeparamref name="T"/> is an enum.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <returns><c>true</c> if <typeparamref name="T"/> is an enum; otherwise <c>false</c>.</returns>
+        public static bool IsEnum<T>() {
+            return IsEnum(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns whether the specified <paramref name="type"/> is an enum.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <returns><c>true</c> if <paramref name="type"/> is an enum; otherwise <c>false</c>.</returns>
+        public static bool IsEnum(Type type) {
+            
+#if NET_FRAMEWORK
+            return type.IsEnum;
+#endif
+            
+#if NET_STANDARD
+            return type.GetTypeInfo().IsEnum;
+#endif
+
+        }
+
+    }
+
+}
