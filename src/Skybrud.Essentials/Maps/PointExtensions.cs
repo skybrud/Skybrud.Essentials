cfe1d9f9af0c78b84075ace4282b6b83b16a96ab
Anders Bjerner <abjerner@skybrud.dk>
2020-09-27 15:08:19 +0200

    Added "GetDistance" extension methods to the "PointExtensions" class

--- src/Skybrud.Essentials/Maps/PointExtensions.cs
+++ src/Skybrud.Essentials/Maps/PointExtensions.cs
@@ -7,6 +7,28 @@ namespace Skybrud.Essentials.Maps {
     /// Class with various extension methods for and related to <see cref="IPoint"/>.
     /// </summary>
     public static class PointExtensions {
+
+        /// <summary>
+        /// Calculates the distance in metres between two GPS points.
+        /// </summary>
+        /// <param name="point">The first point.</param>
+        /// <param name="otherPoint">The second point.</param>
+        /// <returns>The distance in metres between the two points.</returns>
+        /// <remarks>The distance is calculated using the <see cref="EarthConstants.EquatorialRadius"/> of Earth.</remarks>
+        public static double GetDistance(this IPoint point, IPoint otherPoint) {
+            return PointUtils.GetDistance(point, otherPoint);
+        }
+
+        /// <summary>
+        /// Calculates the distance in metres between two GPS points on a spheroid.
+        /// </summary>
+        /// <param name="point">The first point.</param>
+        /// <param name="otherPoint">The second point.</param>
+        /// <param name="radius">The equatorial radius of the spheroid.</param>
+        /// <returns>The distance in metres between the two points.</returns>
+        public static double GetDistance(this IPoint point, IPoint otherPoint, double radius) {
+            return PointUtils.GetDistance(point, otherPoint, radius);
+        }
         
         /// <summary>
         /// Returns whether the specified <paramref name="point"/> is equal to <strong>Null Island</strong>, that is
