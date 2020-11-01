9c513486ef2a2a873b489cee336fbf3cc89faa97
Anders Bjerner <abjerner@skybrud.dk>
2020-11-01 16:36:40 +0100

    Introduced new class with NameValueCollection extension methods

---
+++ src/Skybrud.Essentials/Collections/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Skybrud.Essentials.Strings;
+
+namespace Skybrud.Essentials.Collections.Extensions {
+
+#if NET_FRAMEWORK
+
+    /// <summary>
+    /// Class with various extension methods for <see cref="NameValueCollection"/>.
+    /// </summary>
+    public static class NameValueCollectionExtensions {
+
+        /// <summary>
+        /// Returns the boolean value of the item with <paramref name="key"/>. If an matching isn't found, or conversion to a boolean representation failed, <c>false</c> is returned instead.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <returns>The converted value of the item with <paramref name="key"/> if the conversion was successful; otherwise <c>false</c>.</returns>
+        public static bool GetBoolean(this NameValueCollection collection, string key) {
+            return StringUtils.ParseBoolean(collection[key]);
+        }
+
+        /// <summary>
+        /// Returns the boolean value of the item with <paramref name="key"/>. If an matching isn't found, or conversion to a boolean representation failed, <paramref name="fallback"/> is returned instead.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <param name="fallback">The fallback value that will be returned if the conversion fails.</param>
+        /// <returns>The converted value of the item with <paramref name="key"/> if the conversion was successful; otherwise <paramref name="fallback"/>.</returns>
+        public static bool GetBoolean(this NameValueCollection collection, string key, bool fallback) {
+            return StringUtils.ParseBoolean(collection[key], fallback);
+        }
+
+        /// <summary>
+        /// Gets the boolean value associated with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <param name="result">When this method returns, contains the boolean value associated with the specified key, if the key is found and the the conversion succeeded; otherwise <c>false</c>.</param>
+        /// <returns><c>true</c> if an item with <paramref name="key"/> was found converted successfully; otherwise, <c>false.</c></returns>
+        public static bool TryGetBoolean(this NameValueCollection collection, string key, out bool result) {
+            return StringUtils.TryParseBoolean(collection[key], out result);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="int"/> value of the item with the specified <paramref name="key"/>, or <c>0</c> if a matching item couldn't be found or the value couldn't be parsed to an integer.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <returns>The value as an integer (<see cref="int"/>).</returns>
+        public static int GetInt32(this NameValueCollection collection, string key) {
+            return int.TryParse(collection[key], out int value) ? value : 0;
+        }
+
+        /// <summary>
+        ///  Returns the <see cref="int"/> value of the item with the specified <paramref name="key"/>, or <c>0</c> if a matching item couldn't be found or the value couldn't be parsed to an integer.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <param name="fallback">The fallback value that will be returned if the conversion fails.</param>
+        /// <returns>The value as an integer (<see cref="int"/>).</returns>
+        public static int GetInt32(this NameValueCollection collection, string key, int fallback) {
+            return int.TryParse(collection[key], out int value) ? value : fallback;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="int"/> value associated with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <param name="result">When this method returns, contains the <see cref="int"/> value associated with the specified key, if the key is found and the the conversion succeeded; otherwise <c>0</c>.</param>
+        /// <returns><c>true</c> if an item with <paramref name="key"/> was found converted successfully; otherwise, <c>false.</c></returns>
+        public static bool TryGetInt32(this NameValueCollection collection, string key, out int result) {
+            return int.TryParse(collection[key], out result);
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <param name="result">When this method returns, contains the value associated with the specified key, if the key is found; otherwise <c>null</c>.</param>
+        /// <returns><c>true</c> if the <see cref="NameValueCollection"/> contains an element with the specified key; otherwise, <c>false</c>.</returns>
+        public static bool TryGetValue(this NameValueCollection collection, string key, out string result) {
+            result = collection[key];
+            return result != null;
+        }
+
+        /// <summary>
+        /// Returns whether <paramref name="collection"/> contains an item with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key of the item.</param>
+        /// <returns><c>true</c> if an item with <paramref name="key"/> is found; otherwise, <c>false</c>.</returns>
+        public static bool ContainsKey(this NameValueCollection collection, string key) {
+            return collection.AllKeys.Any(x => x == key);
+        }
+
+    }
+
+#endif
+
+}
---
+++ src/UnitTestProject1/Collections/NameValueCollectionTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Collections;
+using Skybrud.Essentials.Collections.Extensions;
+
+namespace UnitTestProject1.Collections {
+
+    [TestClass]
+    public class NameValueCollectionTests {
+        
+        [TestMethod]
+        public void GetBoolean() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "true" },
+                {"c", "false" }
+            };
+
+            Assert.AreEqual(false, nvc.GetBoolean("a"), "#1");
+            Assert.AreEqual(true, nvc.GetBoolean("b"), "#2");
+            Assert.AreEqual(false, nvc.GetBoolean("c"), "#3");
+
+        }
+        
+        [TestMethod]
+        public void GetBooleanWithFallback() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "true" },
+                {"c", "false" }
+            };
+
+            Assert.AreEqual(true, nvc.GetBoolean("a", true), "#1");
+            Assert.AreEqual(true, nvc.GetBoolean("b", true), "#2");
+            Assert.AreEqual(false, nvc.GetBoolean("c", true), "#3");
+
+        }
+        
+        [TestMethod]
+        public void TryGetBoolean() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "true" },
+                {"c", "false" }
+            };
+
+            Assert.AreEqual(false, nvc.TryGetBoolean("a", out bool result1));
+            Assert.AreEqual(false, result1);
+
+            Assert.AreEqual(true, nvc.TryGetBoolean("b", out bool result2));
+            Assert.AreEqual(true, result2);
+
+            Assert.AreEqual(true, nvc.TryGetBoolean("c", out bool result3));
+            Assert.AreEqual(false, result3);
+
+        }
+        
+        [TestMethod]
+        public void GetInt32() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "1" },
+                {"c", "2" }
+            };
+
+            Assert.AreEqual(0, nvc.GetInt32("a"), "#1");
+            Assert.AreEqual(1, nvc.GetInt32("b"), "#2");
+            Assert.AreEqual(2, nvc.GetInt32("c"), "#3");
+
+        }
+        
+        [TestMethod]
+        public void GetGetInt32WithFallback() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "1" },
+                {"c", "2" }
+            };
+
+            Assert.AreEqual(42, nvc.GetInt32("a", 42), "#1");
+            Assert.AreEqual(1, nvc.GetInt32("b", 42), "#2");
+            Assert.AreEqual(2, nvc.GetInt32("c", 42), "#3");
+
+        }
+        
+        [TestMethod]
+        public void TryGetInt32() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "1" },
+                {"c", "2" }
+            };
+
+            Assert.AreEqual(false, nvc.TryGetInt32("a", out int result1));
+            Assert.AreEqual(0, result1);
+
+            Assert.AreEqual(true, nvc.TryGetInt32("b", out int result2));
+            Assert.AreEqual(1, result2);
+
+            Assert.AreEqual(true, nvc.TryGetInt32("c", out int result3));
+            Assert.AreEqual(2, result3);
+
+        }
+        
+        [TestMethod]
+        public void TryGetValue() {
+
+            NameValueCollection nvc = new NameValueCollection {
+                {"a", "" },
+                {"b", "1" },
+                {"c", "2" }
+            };
+
+            Assert.AreEqual(true, nvc.TryGetValue("a", out string result1));
+            Assert.AreEqual("", result1);
+
+            Assert.AreEqual(true, nvc.TryGetValue("b", out string result2));
+            Assert.AreEqual("1", result2);
+
+            Assert.AreEqual(true, nvc.TryGetValue("c", out string result3));
+            Assert.AreEqual("2", result3);
+
+            Assert.AreEqual(false, nvc.TryGetValue("d", out string result4));
+            Assert.AreEqual(null, result4);
+
+        }
+
+    }
+
+}
