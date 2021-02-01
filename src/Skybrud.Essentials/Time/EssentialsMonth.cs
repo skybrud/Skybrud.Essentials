15351ca52f2ae3db5c9e8e7683bda1cb1c057a52
Anders Bjerner <abjerner@skybrud.dk>
2021-02-01 22:54:52 +0100

    Introduced EssentialsMonth class

---
+++ src/Skybrud.Essentials/Time/EssentialsMonth.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Time {
+    
+    /// <summary>
+    /// Class describing a month.
+    /// </summary>
+    public class EssentialsMonth : EssentialsPeriod {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the year of the month.
+        /// </summary>
+        public int Year { get; }
+
+        /// <summary>
+        /// Gets the ordinal of the month - eg. <c>1</c> for January.
+        /// </summary>
+        public int Month { get; }
+
+        /// <summary>
+        /// Gets the month name in English.
+        /// </summary>
+        public string MonthName => TimeUtils.GetMonthName(Month);
+
+        /// <summary>
+        /// Gets the month name according to the current culture.
+        /// </summary>
+        public string LocalMonthName => TimeUtils.GetLocalMonthName(Month);
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new month based on the specified <paramref name="year"/> and <paramref name="month"/>.
+        ///
+        /// Timestamps for start and end will be calculated using <see cref="TimeZoneInfo.Local"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <param name="month">The month.</param>
+        public EssentialsMonth(int year, int month) : this(year, month, TimeZoneInfo.Local) { }
+
+        /// <summary>
+        /// Initializes a new month based on the specified <paramref name="year"/> and <paramref name="month"/>.
+        ///
+        /// Timestamps for start and end will be calculated using <paramref name="timeZone"/>.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="timeZone"></param>
+        public EssentialsMonth(int year, int month, TimeZoneInfo timeZone) {
+            Year = year;
+            Month = month;
+            Start = new EssentialsTime(year, month, 1, timeZone);
+            End = Start.GetEndOfMonth(timeZone);
+        }
+
+        /// <summary>
+        /// Initializes a new instance for the month containing the specified <paramref name="timestamp"/>.
+        ///
+        /// If <paramref name="timestamp"/> doesn't specify a timestamp (eg. only an offset), the timestamp will be converted to <see cref="TimeZoneInfo.Local"/>.
+        /// </summary>
+        /// <param name="timestamp">A timestamp representing the month to be created.</param>
+        public EssentialsMonth(EssentialsTime timestamp) {
+            
+            if (timestamp == null) throw new ArgumentNullException(nameof(timestamp));
+
+            if (timestamp.TimeZone == null) timestamp = timestamp.ToTimeZone(TimeZoneInfo.Local);
+
+            Year = timestamp.Year;
+            Month = timestamp.Month;
+            Start = timestamp.GetStartOfMonth();
+            End = timestamp.GetEndOfMonth();
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance for the month containing the specified <paramref name="timestamp"/>.
+        ///
+        /// <paramref name="timestamp"/> will be converted to <see cref="TimeZoneInfo.Local"/> prior to calculating the start and end of the month.
+        /// </summary>
+        /// <param name="timestamp">A timestamp representing the month to be created.</param>
+        /// <param name="timeZone"></param>
+        public EssentialsMonth(EssentialsTime timestamp, TimeZoneInfo timeZone)  {
+            
+            if (timestamp == null) throw new ArgumentNullException(nameof(timestamp));
+            if (timeZone == null) throw new ArgumentNullException(nameof(timeZone));
+
+            timestamp = timestamp.ToTimeZone(timeZone);
+
+            Year = timestamp.Year;
+            Month = timestamp.Month;
+            Start = timestamp.GetStartOfMonth();
+            End = timestamp.GetEndOfMonth();
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/UnitTestProject1/Time/Time/EssentialMonthTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Time;
+
+namespace UnitTestProject1.Time.Time {
+    
+    [TestClass]
+    public class EssentialMonthTests {
+
+        [TestMethod]
+        public void ConstructorRomance()  {
+
+            TimeZoneInfo romance = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
+
+            EssentialsTime date01 = new EssentialsTime(2021, 1, 15, TimeSpan.FromHours(1));
+            EssentialsTime date02 = new EssentialsTime(2021, 2, 15, TimeSpan.FromHours(1));
+            EssentialsTime date03 = new EssentialsTime(2021, 3, 15, TimeSpan.FromHours(1));
+            EssentialsTime date04 = new EssentialsTime(2021, 4, 15, TimeSpan.FromHours(2));
+            EssentialsTime date05 = new EssentialsTime(2021, 5, 15, TimeSpan.FromHours(2));
+            EssentialsTime date06 = new EssentialsTime(2021, 6, 15, TimeSpan.FromHours(2));
+            EssentialsTime date07 = new EssentialsTime(2021, 7, 15, TimeSpan.FromHours(2));
+            EssentialsTime date08 = new EssentialsTime(2021, 8, 15, TimeSpan.FromHours(2));
+            EssentialsTime date09 = new EssentialsTime(2021, 9, 15, TimeSpan.FromHours(2));
+            EssentialsTime date10 = new EssentialsTime(2021, 10, 15, TimeSpan.FromHours(2));
+            EssentialsTime date11 = new EssentialsTime(2021, 11, 15, TimeSpan.FromHours(1));
+            EssentialsTime date12 = new EssentialsTime(2021, 12, 15, TimeSpan.FromHours(1));
+
+            EssentialsMonth january = new EssentialsMonth(date01, romance);
+            EssentialsMonth february = new EssentialsMonth(date02, romance);
+            EssentialsMonth march = new EssentialsMonth(date03, romance);
+            EssentialsMonth april = new EssentialsMonth(date04, romance);
+            EssentialsMonth may = new EssentialsMonth(date05, romance);
+            EssentialsMonth june = new EssentialsMonth(date06, romance);
+            EssentialsMonth july = new EssentialsMonth(date07, romance);
+            EssentialsMonth august = new EssentialsMonth(date08, romance);
+            EssentialsMonth september = new EssentialsMonth(date09, romance);
+            EssentialsMonth october = new EssentialsMonth(date10, romance);
+            EssentialsMonth november = new EssentialsMonth(date11, romance);
+            EssentialsMonth december = new EssentialsMonth(date12, romance);
+
+            Assert.AreEqual("2021-01-01T00:00:00+01:00", january.Start.ToString(), "January - Start");
+            Assert.AreEqual("2021-01-31T23:59:59+01:00", january.End.ToString(), "January - End");
+
+            Assert.AreEqual("2021-02-01T00:00:00+01:00", february.Start.ToString(), "February - Start");
+            Assert.AreEqual("2021-02-28T23:59:59+01:00", february.End.ToString(), "February - End");
+
+            Assert.AreEqual("2021-03-01T00:00:00+01:00", march.Start.ToString(), "March - Start");
+            Assert.AreEqual("2021-03-31T23:59:59+02:00", march.End.ToString(), "March - End");
+
+            Assert.AreEqual("2021-04-01T00:00:00+02:00", april.Start.ToString(), "April - Start");
+            Assert.AreEqual("2021-04-30T23:59:59+02:00", april.End.ToString(), "April - End");
+
+            Assert.AreEqual("2021-05-01T00:00:00+02:00", may.Start.ToString(), "May - Start");
+            Assert.AreEqual("2021-05-31T23:59:59+02:00", may.End.ToString(), "May - End");
+
+            Assert.AreEqual("2021-06-01T00:00:00+02:00", june.Start.ToString(), "June - Start");
+            Assert.AreEqual("2021-06-30T23:59:59+02:00", june.End.ToString(), "June - End");
+
+            Assert.AreEqual("2021-07-01T00:00:00+02:00", july.Start.ToString(), "July - Start");
+            Assert.AreEqual("2021-07-31T23:59:59+02:00", july.End.ToString(), "July - End");
+
+            Assert.AreEqual("2021-08-01T00:00:00+02:00", august.Start.ToString(), "August - Start");
+            Assert.AreEqual("2021-08-31T23:59:59+02:00", august.End.ToString(), "August - End");
+
+            Assert.AreEqual("2021-09-01T00:00:00+02:00", september.Start.ToString(), "September - Start");
+            Assert.AreEqual("2021-09-30T23:59:59+02:00", september.End.ToString(), "September - End");
+
+            Assert.AreEqual("2021-10-01T00:00:00+02:00", october.Start.ToString(), "October - Start");
+            Assert.AreEqual("2021-10-31T23:59:59+01:00", october.End.ToString(), "October - End");
+
+            Assert.AreEqual("2021-11-01T00:00:00+01:00", november.Start.ToString(), "November - Start");
+            Assert.AreEqual("2021-11-30T23:59:59+01:00", november.End.ToString(), "November - End");
+
+            Assert.AreEqual("2021-12-01T00:00:00+01:00", december.Start.ToString(), "December - Start");
+            Assert.AreEqual("2021-12-31T23:59:59+01:00", december.End.ToString(), "December - End");
+
+        }
+
+    }
+
+}
