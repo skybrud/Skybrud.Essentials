3bda45b7f6431e83813ef1b88fb1b702f33b17f3
Anders Bjerner <abjerner@skybrud.dk>
2021-03-22 23:05:28 +0100

    StripHtml methods shouldn't break on null values

--- src/UnitTestProject1/Strings/StringTests.cs
+++ src/UnitTestProject1/Strings/StringTests.cs
@@ -468,6 +468,8 @@ namespace UnitTestProject1.Strings {
         public void StripHtml() {
 
             var samples = new[] {
+                new { Input = default(string), Output = "" },
+                new { Input = "", Output = "" },
                 new { Input = "<p>Hello World</p>", Output = "Hello World" },
                 new { Input = "Hello<br />World", Output = "HelloWorld" },
                 new { Input = "<p>Hello World &amp; Goodbye World</p>", Output = "Hello World & Goodbye World" }
@@ -483,6 +485,8 @@ namespace UnitTestProject1.Strings {
         public void StripHtmlIgnore() {
 
             var samples = new[] {
+                new { Input = default(string), Output = "", Ignore = new []{"strong"} },
+                new { Input = "", Output = "", Ignore = new []{"strong"} },
                 new { Input = "<p>Hello <strong>World</strong></p>", Output = "Hello <strong>World</strong>", Ignore = new []{"strong"} },
                 new { Input = "<ul><li><a href=\"#hest\"><strong>Hest</strong><br /></a></li></ul>", Output = "<a href=\"#hest\">Hest</a>", Ignore = new []{"a"} }
             };
