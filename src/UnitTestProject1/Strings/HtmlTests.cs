2947553706c5c7bb7099f1a2be465993bf40f5e1
Anders Bjerner <abjerner@skybrud.dk>
2021-03-22 23:11:00 +0100

    Moving a bit around

---
+++ src/UnitTestProject1/Strings/HtmlTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Strings;
+
+namespace UnitTestProject1.Strings {
+    
+    [TestClass]
+    public class HtmlTests {
+
+        [TestMethod]
+        public void StripHtml() {
+
+            var samples = new[] {
+                new { Input = default(string), Output = "" },
+                new { Input = "", Output = "" },
+                new { Input = "<p>Hello World</p>", Output = "Hello World" },
+                new { Input = "Hello<br />World", Output = "HelloWorld" },
+                new { Input = "<p>Hello World &amp; Goodbye World</p>", Output = "Hello World & Goodbye World" }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Output, StringUtils.StripHtml(sample.Input));
+            }
+
+        }
+
+        [TestMethod]
+        public void StripHtmlIgnore() {
+
+            var samples = new[] {
+                new { Input = default(string), Output = "", Ignore = new []{"strong"} },
+                new { Input = "", Output = "", Ignore = new []{"strong"} },
+                new { Input = "<p>Hello <strong>World</strong></p>", Output = "Hello <strong>World</strong>", Ignore = new []{"strong"} },
+                new { Input = "<ul><li><a href=\"#hest\"><strong>Hest</strong><br /></a></li></ul>", Output = "<a href=\"#hest\">Hest</a>", Ignore = new []{"a"} }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Output, StringUtils.StripHtml(sample.Input, sample.Ignore));
+            }
+
+        }
+
+    }
+
+}
--- src/UnitTestProject1/Strings/StringTests.cs
+++ src/UnitTestProject1/Strings/StringTests.cs
@@ -465,39 +465,6 @@ namespace UnitTestProject1.Strings {
         }
 
         [TestMethod]
-        public void StripHtml() {
-
-            var samples = new[] {
-                new { Input = default(string), Output = "" },
-                new { Input = "", Output = "" },
-                new { Input = "<p>Hello World</p>", Output = "Hello World" },
-                new { Input = "Hello<br />World", Output = "HelloWorld" },
-                new { Input = "<p>Hello World &amp; Goodbye World</p>", Output = "Hello World & Goodbye World" }
-            };
-
-            foreach (var sample in samples) {
-                Assert.AreEqual(sample.Output, StringUtils.StripHtml(sample.Input));
-            }
-
-        }
-
-        [TestMethod]
-        public void StripHtmlIgnore() {
-
-            var samples = new[] {
-                new { Input = default(string), Output = "", Ignore = new []{"strong"} },
-                new { Input = "", Output = "", Ignore = new []{"strong"} },
-                new { Input = "<p>Hello <strong>World</strong></p>", Output = "Hello <strong>World</strong>", Ignore = new []{"strong"} },
-                new { Input = "<ul><li><a href=\"#hest\"><strong>Hest</strong><br /></a></li></ul>", Output = "<a href=\"#hest\">Hest</a>", Ignore = new []{"a"} }
-            };
-
-            foreach (var sample in samples) {
-                Assert.AreEqual(sample.Output, StringUtils.StripHtml(sample.Input, sample.Ignore));
-            }
-
-        }
-
-        [TestMethod]
         public void IsInt32() {
 
             Assert.AreEqual(false, StringUtils.IsInt32(""), "#1");
