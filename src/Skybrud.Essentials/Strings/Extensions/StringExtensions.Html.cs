7f8e778add58abce5df16396ebb9d6ed78b2e9cf
Anders Bjerner <abjerner@skybrud.dk>
2021-03-22 23:31:34 +0100

    Moving a bit around

---
+++ src/Skybrud.Essentials/Strings/Extensions/StringExtensions.Html.cs
@@ -0,0 +1,27 @@
+﻿namespace Skybrud.Essentials.Strings.Extensions {
+
+    public static partial class StringExtensions {
+      
+        /// <summary>
+        /// Strips all HTML elements from the specified <paramref name="html"/> string.
+        /// </summary>
+        /// <param name="html">The input string containing HTML.</param>
+        /// <returns>The input string without HTML markup.</returns>
+        public static string StripHtml(this string html) {
+            return StringUtils.StripHtml(html);
+        }
+
+        /// <summary>
+        /// Strips all HTML elements from the specified <paramref name="html"/> string, but keeps the HTML tags as
+        /// specified in <paramref name="ignore"/>.
+        /// </summary>
+        /// <param name="html">The input string containing the HTML.</param>
+        /// <param name="ignore">An of tag names (without the brackets, like <c>div</c>) to ignore.</param>
+        /// <returns>The stripped result.</returns>
+        public static string StripHtml(this string html, params string[] ignore) {
+            return StringUtils.StripHtml(html, ignore);
+        }
+
+    }
+
+}
--- src/Skybrud.Essentials/Strings/Extensions/StringExtensions.cs
+++ src/Skybrud.Essentials/Strings/Extensions/StringExtensions.cs
@@ -78,26 +78,6 @@ namespace Skybrud.Essentials.Strings.Extensions {
         }
 
         /// <summary>
-        /// Strips all HTML elements from the specified <paramref name="html"/> string.
-        /// </summary>
-        /// <param name="html">The input string containing HTML.</param>
-        /// <returns>The input string without HTML markup.</returns>
-        public static string StripHtml(this string html) {
-            return StringUtils.StripHtml(html);
-        }
-
-        /// <summary>
-        /// Strips all HTML elements from the specified <paramref name="html"/> string, but keeps the HTML tags as
-        /// specified in <paramref name="ignore"/>.
-        /// </summary>
-        /// <param name="html">The input string containing the HTML.</param>
-        /// <param name="ignore">An of tag names (without the brackets, like <c>div</c>) to ignore.</param>
-        /// <returns>The stripped result.</returns>
-        public static string StripHtml(this string html, params string[] ignore) {
-            return StringUtils.StripHtml(html, ignore);
-        }
-
-        /// <summary>
         /// Gets whether the <paramref name="input"/> string has a value. This extension method is equal to calling
         /// <see cref="string.IsNullOrWhiteSpace"/>.
         /// </summary>
