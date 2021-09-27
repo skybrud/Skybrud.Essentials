dec4198433a251b6624c0d21b2baedacb085ed99
Anders Bjerner <abjerner@skybrud.dk>
2021-09-27 22:07:06 +0200

    Unit test should specify explicit culture
    If a culture or format provider isn't specified for the EssentialsDate.TryParse method, the format is based on the current culture, which may differ from environment to environment, and thereby give different results :(

--- src/UnitTestProject1/Time/EssentialsDateTests.cs
+++ src/UnitTestProject1/Time/EssentialsDateTests.cs
@@ -26,17 +26,48 @@ namespace UnitTestProject1.Time {
         [TestMethod]
         public void TryParse() {
 
-            bool success1 = EssentialsDate.TryParse("2019-08-17", out EssentialsDate result1);
-            bool success2 = EssentialsDate.TryParse("08/17/2019", out EssentialsDate result2);
+            bool success1 = EssentialsDate.TryParse("2019-08-17", CultureInfo.InvariantCulture, DateTimeStyles.None, out EssentialsDate result1);
+            bool success2 = EssentialsDate.TryParse("08/17/2019", CultureInfo.InvariantCulture, DateTimeStyles.None, out EssentialsDate result2);
+            bool success3 = EssentialsDate.TryParse("17/08/2019", CultureInfo.InvariantCulture, DateTimeStyles.None, out EssentialsDate result3);
 
-            Assert.AreEqual(true, success1);
-            Assert.AreEqual(false, success2);
+            Assert.AreEqual(true, success1, "#1");
+            Assert.AreEqual(true, success2, "#2");
+            Assert.AreEqual(false, success3, "#3");
 
             Assert.AreEqual(2019, result1.Year);
             Assert.AreEqual(8, result1.Month);
             Assert.AreEqual(17, result1.Day);
 
+            Assert.AreEqual(2019, result2.Year);
+            Assert.AreEqual(8, result2.Month);
+            Assert.AreEqual(17, result2.Day);
+
+            Assert.IsNull(result3);
+
+        }
+
+        [TestMethod]
+        public void TryParseDanish() {
+
+            CultureInfo culture = CultureInfo.GetCultureInfo("da-DK");
+
+            bool success1 = EssentialsDate.TryParse("2019-08-17", culture, DateTimeStyles.None, out EssentialsDate result1);
+            bool success2 = EssentialsDate.TryParse("08/17/2019", culture, DateTimeStyles.None, out EssentialsDate result2);
+            bool success3 = EssentialsDate.TryParse("17/08/2019", culture, DateTimeStyles.None, out EssentialsDate result3);
+
+            Assert.AreEqual(true, success1, "#1");
+            Assert.AreEqual(false, success2, "#2");
+            Assert.AreEqual(true, success3, "#3");
+
+            Assert.AreEqual(2019, result1.Year);
+            Assert.AreEqual(8, result1.Month);
+            Assert.AreEqual(17, result1.Day);
+            
             Assert.IsNull(result2);
+
+            Assert.AreEqual(2019, result3.Year);
+            Assert.AreEqual(8, result3.Month);
+            Assert.AreEqual(17, result3.Day);
 
         }
 
