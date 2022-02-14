5f8132a5b0a101a7220f695e633bc0cde27c2782
Anders Bjerner <abjerner@skybrud.dk>
2022-02-14 13:42:07 +0100

    Cleaned up the UnixTimeUtils class a bit
    Instead of initializing a DateTime or DateTimeOffset for the start of the Unix epoch everytime these methods are called, a constant (readonly field) is now used instead. This also makes the code easier to read.

--- src/Skybrud.Essentials/Time/UnixTime/UnixTimeUtils.cs
+++ src/Skybrud.Essentials/Time/UnixTime/UnixTimeUtils.cs
@@ -7,15 +7,35 @@ namespace Skybrud.Essentials.Time.UnixTime {
     /// </summary>
     public static class UnixTimeUtils {
 
+        #region Constants
+        
+        /// <summary>
+        /// Gets an instance of <see cref="DateTime"/> representing the start of the Unix epoch.
+        /// </summary>
+        private static readonly DateTime DateTimeUnixTimeStartUtc = new (1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        
+        /// <summary>
+        /// Gets an instance of <see cref="DateTimeOffset"/> representing the start of the Unix epoch.
+        /// </summary>
+        private static readonly DateTimeOffset DateTimeOffsetUnixTimeStartUtc = new (1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+
+        #endregion
+
+        #region Properties
+
         /// <summary>
         /// Returns the amount of seconds since the start of the Unix epoch - that is <c>1st of January, 1970 - 00:00:00 GMT</c>.
         /// </summary>
-        public static double CurrentSeconds => (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        public static double CurrentSeconds => (DateTime.UtcNow - DateTimeUnixTimeStartUtc).TotalSeconds;
 
         /// <summary>
         /// Returns the amount of milliseconds since the start of the Unix epoch - that is <c>1st of January, 1970 - 00:00:00 GMT</c>.
         /// </summary>
-        public static double CurrentMilliseconds => (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+        public static double CurrentMilliseconds => (DateTime.UtcNow - DateTimeUnixTimeStartUtc).TotalMilliseconds;
+
+        #endregion
+
+        #region Member methods
 
         /// <summary>
         /// Returns an instance of <see cref="DateTimeOffset"/> based on the amount of seconds since the start of the
@@ -24,7 +44,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="seconds">The Unix timestamp specified in seconds.</param>
         /// <returns>The timestamp as an instance of <see cref="DateTimeOffset"/>.</returns>
         public static DateTimeOffset FromSeconds(int seconds) {
-            return new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero).AddSeconds(seconds);
+            return DateTimeOffsetUnixTimeStartUtc.AddSeconds(seconds);
         }
 
         /// <summary>
@@ -34,7 +54,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="seconds">The Unix timestamp specified in seconds.</param>
         /// <returns>The timestamp as an instance of <see cref="DateTimeOffset"/>.</returns>
         public static DateTimeOffset FromSeconds(long seconds) {
-            return new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero).AddSeconds(seconds);
+            return DateTimeOffsetUnixTimeStartUtc.AddSeconds(seconds);
         }
 
         /// <summary>
@@ -44,7 +64,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="seconds">The Unix timestamp specified in seconds.</param>
         /// <returns>The timestamp as an instance of <see cref="DateTimeOffset"/>.</returns>
         public static DateTimeOffset FromSeconds(double seconds) {
-            return new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero).AddSeconds(seconds);
+            return DateTimeOffsetUnixTimeStartUtc.AddSeconds(seconds);
         }
 
         /// <summary>
@@ -54,7 +74,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="seconds">The Unix timestamp specified in seconds.</param>
         /// <returns>The timestamp as an instance of <see cref="DateTimeOffset"/>.</returns>
         public static DateTimeOffset FromSeconds(string seconds) {
-            return new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero).AddSeconds(long.Parse(seconds));
+            return DateTimeOffsetUnixTimeStartUtc.AddSeconds(long.Parse(seconds));
         }
 
         /// <summary>
@@ -64,7 +84,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="timestamp">The isntance of <see cref="DateTime"/> the timestamp should be based on.</param>
         /// <returns>An instance of <see cref="double"/> representing the Unix timestamp.</returns>
         public static double ToSeconds(DateTime timestamp) {
-            return (timestamp.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+            return (timestamp.ToUniversalTime() - DateTimeUnixTimeStartUtc).TotalSeconds;
         }
 
         /// <summary>
@@ -74,7 +94,7 @@ namespace Skybrud.Essentials.Time.UnixTime {
         /// <param name="timestamp">The isntance of <see cref="DateTimeOffset"/> the timestamp should be based on.</param>
         /// <returns>An instance of <see cref="double"/> representing the Unix timestamp.</returns>
         public static double ToSeconds(DateTimeOffset timestamp) {
-            return (timestamp.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero)).TotalSeconds;
+            return (timestamp.ToUniversalTime() - DateTimeOffsetUnixTimeStartUtc).TotalSeconds;
         }
 
     }
