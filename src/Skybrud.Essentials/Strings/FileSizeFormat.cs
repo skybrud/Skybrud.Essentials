1b1544316dbc75a2395a5c0fcb2f18789ee8a115
Anders Bjerner <abjerner@skybrud.dk>
2022-07-04 18:38:27 +0200

    Introduced new "StringUtils.FormatFileSize" methods
    This adds a way to format a given file size in to either kilobytes, kibibytes or a mix of the to.
    
    Implements #29

---
+++ src/Skybrud.Essentials/Strings/FileSizeFormat.cs
@@ -0,0 +1,35 @@
+﻿namespace Skybrud.Essentials.Strings {
+    
+    /// <summary>
+    /// Enum class indicating the format in which a file size should be formatted.
+    /// </summary>
+    public enum FileSizeFormat {
+
+        /// <summary>
+        /// Indicates that the file size should be formatted using kibibytes (based on the power of 2), but using units
+        /// like <c>KB</c>, <c>MB</c> etc.
+        ///
+        /// Teachnically this is wrong as 1 kibibyte (1024 bytes) should use the unit <c>KiB</c>, as <c>KB</c> refers
+        /// to kilobytes. But <c>KB</c> is commonly used to represent kibibytes, thereby causing some ambiguity.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Indicates that the file size should be formatted using kibibytes (based on the power of 2) - eg.
+        /// <c>KiB</c>, <c>MiB</c> and so forth.
+        ///
+        /// Using this format, <c>1 KiB</c> equal to <c>1024 bytes</c>.
+        /// </summary>
+        Kibi,
+
+        /// <summary>
+        /// Indicates that the file size should be formatted using kilobytes (based on the power of 10) - eg.
+        /// <c>KB</c>, <c>MB</c> and so forth.
+        ///
+        /// Using this format, <c>1 KB</c> equal to <c>1000 bytes</c>.
+        /// </summary>
+        Kilo
+
+    }
+
+}
