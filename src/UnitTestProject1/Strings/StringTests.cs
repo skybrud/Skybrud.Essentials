749f9bb2b55aefafaa285a7f0136dda86b4e1822
Anders Bjerner <abjerner@skybrud.dk>
2022-08-27 14:55:09 +0200

    Cleanup

--- src/UnitTestProject1/Strings/StringTests.cs
+++ src/UnitTestProject1/Strings/StringTests.cs
@@ -3,7 +3,6 @@ using System.Globalization;
 using System.Net;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Strings;
 using Skybrud.Essentials.Strings.Extensions;
 
