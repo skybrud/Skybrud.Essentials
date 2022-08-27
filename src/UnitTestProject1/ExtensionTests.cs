5b17b47d75591e1401a81b5b759c17d1b057e351
Anders Bjerner <abjerner@skybrud.dk>
2022-08-27 21:20:26 +0200

    Added unit test to check that all extension methods are in fact extension methods

---
+++ src/UnitTestProject1/ExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Reflection.Extensions;
+using Skybrud.Essentials.Time;
+
+namespace UnitTestProject1 {
+
+    [TestClass]
+    public class ExtensionTests {
+
+        [TestMethod]
+        public void PublicMethodMustHaveThisKeyword() {
+
+            foreach (var type in typeof(EssentialsTime).Assembly.GetTypes().Where(x => x.IsExtensionClass())) {
+
+                foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Static)) {
+
+                    Assert.IsTrue(method.IsExtensionMethod(), $"Public static method is missing 'this' keyword.\r\n\r\nType: " + type + "\r\n" + method);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
