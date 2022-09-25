8e75a29ce4b1ea9c5a51fcab0eb478db385b452b
Anders Bjerner <abjerner@skybrud.dk>
2022-09-25 23:12:32 +0200

    Introduced new NewtonsoftJsonObjectExtensions class
    See #42

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/Extensions/NewtonsoftJsonObjectExtensions.Boolean.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Strings;
+using Skybrud.Essentials.Strings.Extensions;
+
+// ReSharper disable SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+
+namespace Skybrud.Essentials.Json.Newtonsoft.Extensions {
+
+    public static partial class NewtonsoftJsonObjectExtensions {
+
+        /// <summary>
+        /// Returns the <see cref="bool"/> value of the property with the specified <paramref name="propertyName"/>. If
+        /// a matching property can not be found or the value can not be successfully converted to a <see cref="bool"/>
+        /// value, <c>false</c> is returned instead.
+        /// </summary>
+        /// <param name="json">The parent JSON object.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>An instance of <see cref="bool"/>.</returns>
+        public static bool GetBoolean(this JObject json, string propertyName) {
+            return GetBoolean(json, propertyName, false);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="bool"/> value of the property with the specified <paramref name="propertyName"/>. If
+        /// a matching property can not be found or the value can not be successfully converted to a <see cref="bool"/>
+        /// value, <paramref name="fallback"/> is returned instead.
+        /// </summary>
+        /// <param name="json">The parent JSON object.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="fallback">The fallback value.</param>
+        /// <returns>An instance of <see cref="bool"/>.</returns>
+        public static bool GetBoolean(this JObject json, string propertyName, bool fallback) {
+            JToken token = json?[propertyName];
+            return token?.Type switch {
+                JTokenType.Boolean => token.ToObject<bool>(),
+                JTokenType.Integer => token.ToObject<int>() switch {
+                    0 => false,
+                    1 => true,
+                    _ => fallback
+                },
+                JTokenType.String => token.ToObject<string>().ToBoolean(fallback),
+                _ => fallback
+            };
+        }
+
+
+        /// <summary>
+        /// Attempts to get a boolean value from the property with the specified <paramref name="propertyName"/>.
+        /// </summary>
+        /// <param name="json">The parent JSON object.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="result">When this method returns, if the conversion succeeded, contains the parsed boolean value. If the conversion failed, contains <c>false</c>.</param>
+        /// <returns><c>true</c> if value was converted successfully; otherwise, <c>false</c>.</returns>
+        public static bool TryGetBoolean(this JObject json, string propertyName, out bool result) {
+
+            if (TryGetBoolean(json, propertyName, out bool? temp)) {
+                result = temp!.Value;
+                return true;
+            }
+
+            result = default;
+            return false;
+
+        }
+
+        /// <summary>
+        /// Attempts to get a boolean value from the property with the specified <paramref name="propertyName"/>.
+        /// </summary>
+        /// <param name="json">The parent JSON object.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="result">When this method returns, if the conversion succeeded, contains the parsed boolean value. If the conversion failed, contains <c>null</c>.</param>
+        /// <returns><c>true</c> if value was converted successfully; otherwise, <c>false</c>.</returns>
+        public static bool TryGetBoolean(this JObject json, string propertyName, out bool? result) {
+
+            JToken token = json?[propertyName];
+
+            switch (token?.Type) {
+
+                case JTokenType.Boolean:
+                    result = token.ToObject<bool>();
+                    return true;
+
+                case JTokenType.Integer:
+
+                    switch (token.ToObject<int>()) {
+                        case 0:
+                            result = false;
+                            return true;
+                        case 1:
+                            result = true;
+                            return true;
+                        default:
+                            result = null;
+                            return true;
+                    }
+
+                case JTokenType.String:
+                    return StringUtils.TryParseBoolean(token.ToObject<string>(), out result);
+
+                default:
+                    result = null;
+                    return false;
+
+            }
+
+        }
+
+    }
+
+}
---
+++ src/Skybrud.Essentials/Json/Newtonsoft/Extensions/NewtonsoftJsonObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Skybrud.Essentials.Json.Newtonsoft.Extensions {
+
+    /// <summary>
+    /// Static class with various extension methods for <see cref="JObject"/>.
+    /// </summary>
+    public static partial class NewtonsoftJsonObjectExtensions { }
+
+}
