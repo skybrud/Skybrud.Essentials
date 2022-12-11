9fc08fbd84d002e118d2a95dec2e5bc0abc685e5
Anders Bjerner <abjerner@skybrud.dk>
2022-12-11 20:32:18 +0100

    Added "ToString", "Parse" and "TryParse" methods to the "EssentialsWeekRange" class
    Implements #34

--- src/Skybrud.Essentials/Time/EssentialsWeekRange.cs
+++ src/Skybrud.Essentials/Time/EssentialsWeekRange.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Skybrud.Essentials.Strings;
+using Skybrud.Essentials.Strings.Extensions;
 
 namespace Skybrud.Essentials.Time {
 
@@ -72,6 +77,14 @@ namespace Skybrud.Essentials.Time {
         #region Member methods
 
         /// <summary>
+        /// Returns a string representation of the week range - eg. <c>2022-W49__2023-W01</c>.
+        /// </summary>
+        /// <returns>An instance of <see cref="string"/> representing the week range.</returns>
+        public override string ToString() {
+            return $"{Weeks[0]}__{Weeks[Weeks.Length - 1]}";
+        }
+
+        /// <summary>
         /// Returns an enumerator that iterates through the <see cref="EssentialsWeek"/> of the range.
         /// </summary>
         /// <returns>An enumerator that can be used to iterate through the <see cref="EssentialsWeek"/> of the range.</returns>
@@ -81,6 +94,52 @@ namespace Skybrud.Essentials.Time {
 
         IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Parses the specified <paramref name="input"/> string representation of a week range into a corresponding of
+        /// <see cref="EssentialsWeekRange"/>. If <paramref name="input"/> is either <see langword="null"/> or white space,
+        /// <see langword="null"/> will be returned instead.
+        /// </summary>
+        /// <param name="input">The string identifying the week range.</param>
+        /// <returns>An instance of <see cref="EssentialsWeekRange"/>.</returns>
+        public static EssentialsWeekRange? Parse(string? input) {
+            if (string.IsNullOrWhiteSpace(input)) return null;
+            if (TryParse(input!, out EssentialsWeekRange? result)) return result;
+            throw new Exception("Specified input string is not a valid week.");
+        }
+
+        /// <summary>
+        /// Tries to convert the specified string representation of a week range to its <see cref="EssentialsWeekRange"/>
+        /// equivalent, and returns a value that indicates whether the conversion succeeded.
+        /// </summary>
+        /// <param name="input">A string that contains the week range to convert.</param>
+        /// <param name="result">When the method returns, contains the <see cref="EssentialsWeekRange"/> value
+        /// equivalent to the week range, if the conversion succeeded, or <see langword="null"/>, if the conversion
+        /// failed. The conversion fails if the input parameter is <see langword="null"/> or does not contain a
+        /// valid string representation of a date. This parameter is passed uninitialized.</param>
+        /// <returns><see langword="true"/> if the input parameter is successfully converted; otherwise, <see langword="false"/>.</returns>
+        public static bool TryParse(string? input, [NotNullWhen(true)] out EssentialsWeekRange? result) {
+
+            if (string.IsNullOrWhiteSpace(input)) {
+                result = null;
+                return false;
+            }
+
+            if (RegexUtils.IsMatch(input, "^([0-9]{4})(-|-W|W)([0-9]{1,2})(_|__)([0-9]{4})(-|-W|W)([0-9]{1,2})$", out Match match)) {
+                var startWeek = new EssentialsWeek(match.Groups[1].Value.ToInt32(), match.Groups[3].Value.ToInt32());
+                var endWeek = new EssentialsWeek(match.Groups[5].Value.ToInt32(), match.Groups[7].Value.ToInt32());
+                result = new EssentialsWeekRange(startWeek, endWeek);
+                return true;
+            }
+
+            result = null;
+            return false;
+
         }
 
         #endregion
---
+++ src/UnitTestProject1/Time/Time/EssentialsWeekRangeTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Time;
+
+// ReSharper disable EqualExpressionComparison
+
+#pragma warning disable CS1718
+
+namespace UnitTestProject1.Time.Time {
+
+    [TestClass]
+    public class EssentialsWeekRangeTests {
+
+        [TestMethod]
+        public new void ToString() {
+
+            EssentialsWeek week1 = new EssentialsWeek(2022, 49);
+            EssentialsWeek week2 = new EssentialsWeek(2023, 1);
+
+            EssentialsWeekRange range1 = new EssentialsWeekRange(week1, week2);
+            EssentialsWeekRange range2 = new EssentialsWeekRange(week2, week1);
+
+            Assert.AreEqual("2022-W49", week1.ToString());
+            Assert.AreEqual("2023-W01", week2.ToString());
+            Assert.AreEqual("2022-W49__2023-W01", range1.ToString());
+            Assert.AreEqual("2023-W01__2022-W49", range2.ToString());
+
+        }
+
+        [TestMethod]
+        public void Parse() {
+
+            EssentialsWeekRange range1 = EssentialsWeekRange.Parse(null);
+            EssentialsWeekRange range2 = EssentialsWeekRange.Parse(string.Empty);
+            EssentialsWeekRange range3 = EssentialsWeekRange.Parse("2022-W49__2023-W01");
+            EssentialsWeekRange range4 = EssentialsWeekRange.Parse("2023-W01__2022-W49");
+
+            Assert.IsNull(range1);
+            Assert.IsNull(range2);
+
+            Assert.IsNotNull(range3);
+            Assert.IsFalse(range3.IsReverse);
+            Assert.AreEqual("2022-W49", range3.Weeks[0].ToString());
+            Assert.AreEqual("2023-W01", range3.Weeks[range3.Weeks.Length - 1].ToString());
+
+            Assert.IsNotNull(range4);
+            Assert.IsTrue(range4.IsReverse);
+            Assert.AreEqual("2023-W01", range4.Weeks[0].ToString());
+            Assert.AreEqual("2022-W49", range4.Weeks[range4.Weeks.Length - 1].ToString());
+
+        }
+
+    }
+
+}
