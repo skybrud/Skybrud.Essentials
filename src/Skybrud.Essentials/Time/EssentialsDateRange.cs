7e7d0edefe4284255ba642230b562185889e284d
Anders Bjerner <abjerner@skybrud.dk>
2022-12-11 20:41:43 +0100

    Added "ToString", "Parse" and "TryParse" methods to the "EssentialsDateRange" class
    Implements #32

--- src/Skybrud.Essentials/Time/EssentialsDateRange.cs
+++ src/Skybrud.Essentials/Time/EssentialsDateRange.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Skybrud.Essentials.Strings;
+using Skybrud.Essentials.Strings.Extensions;
 
 namespace Skybrud.Essentials.Time {
 
@@ -66,6 +71,14 @@ namespace Skybrud.Essentials.Time {
         #region Member methods
 
         /// <summary>
+        /// Returns a string representation of the week range - eg. <c>2022-10-01__2022-12-31</c>.
+        /// </summary>
+        /// <returns>An instance of <see cref="string"/> representing the week range.</returns>
+        public override string ToString() {
+            return $"{Start}__{End}";
+        }
+
+        /// <summary>
         /// Returns an enumerator that iterates through the <see cref="EssentialsDate"/> of the range.
         /// </summary>
         /// <returns>An enumerator that can be used to iterate through the <see cref="EssentialsDate"/> of the range.</returns>
@@ -75,6 +88,52 @@ namespace Skybrud.Essentials.Time {
 
         IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Parses the specified <paramref name="input"/> string representation of a date range into a corresponding of
+        /// <see cref="EssentialsDateRange"/>. If <paramref name="input"/> is either <see langword="null"/> or white space,
+        /// <see langword="null"/> will be returned instead.
+        /// </summary>
+        /// <param name="input">The string identifying the date range.</param>
+        /// <returns>An instance of <see cref="EssentialsDateRange"/>.</returns>
+        public static EssentialsDateRange? Parse(string? input) {
+            if (string.IsNullOrWhiteSpace(input)) return null;
+            if (TryParse(input!, out EssentialsDateRange? result)) return result;
+            throw new Exception("Specified input string is not a valid date range.");
+        }
+
+        /// <summary>
+        /// Tries to convert the specified string representation of a date range to its <see cref="EssentialsDateRange"/>
+        /// equivalent, and returns a value that indicates whether the conversion succeeded.
+        /// </summary>
+        /// <param name="input">A string that contains the date range to convert.</param>
+        /// <param name="result">When the method returns, contains the <see cref="EssentialsDateRange"/> value
+        /// equivalent to the date range, if the conversion succeeded, or <see langword="null"/>, if the conversion
+        /// failed. The conversion fails if the input parameter is <see langword="null"/> or does not contain a
+        /// valid string representation of a date range. This parameter is passed uninitialized.</param>
+        /// <returns><see langword="true"/> if the input parameter is successfully converted; otherwise, <see langword="false"/>.</returns>
+        public static bool TryParse(string? input, [NotNullWhen(true)] out EssentialsDateRange? result) {
+
+            if (string.IsNullOrWhiteSpace(input)) {
+                result = null;
+                return false;
+            }
+
+            if (RegexUtils.IsMatch(input, "^([0-9]{4})-([0-9]{2})-([0-9]{2})(_|__)([0-9]{4})-([0-9]{2})-([0-9]{2})$", out Match match)) {
+                var startDate = new EssentialsDate(match.Groups[1].Value.ToInt32(), match.Groups[2].Value.ToInt32(), match.Groups[3].Value.ToInt32());
+                var endDate = new EssentialsDate(match.Groups[5].Value.ToInt32(), match.Groups[6].Value.ToInt32(), match.Groups[7].Value.ToInt32());
+                result = new EssentialsDateRange(startDate, endDate);
+                return true;
+            }
+
+            result = null;
+            return false;
+
         }
 
         #endregion
---
+++ src/UnitTestProject1/Time/Time/EssentialsDateRangeTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skybrud.Essentials.Time;
+
+namespace UnitTestProject1.Time.Time {
+
+    [TestClass]
+    public class EssentialsDateRangeTests {
+
+        [TestMethod]
+        public new void ToString() {
+
+            EssentialsDate date1 = new EssentialsDate(2022, 10, 1);
+            EssentialsDate date2 = new EssentialsDate(2022, 12, 31);
+
+            EssentialsDateRange range1 = new EssentialsDateRange(date1, date2);
+            EssentialsDateRange range2 = new EssentialsDateRange(date2, date1);
+
+            Assert.AreEqual("2022-10-01", date1.ToString());
+            Assert.AreEqual("2022-12-31", date2.ToString());
+            Assert.AreEqual("2022-10-01__2022-12-31", range1.ToString());
+            Assert.AreEqual("2022-12-31__2022-10-01", range2.ToString());
+
+        }
+
+        [TestMethod]
+        public void Parse() {
+
+            EssentialsDateRange range1 = EssentialsDateRange.Parse(null);
+            EssentialsDateRange range2 = EssentialsDateRange.Parse(string.Empty);
+            EssentialsDateRange range3 = EssentialsDateRange.Parse("2022-10-01__2022-12-31");
+            EssentialsDateRange range4 = EssentialsDateRange.Parse("2022-12-31__2022-10-01");
+
+            Assert.IsNull(range1);
+            Assert.IsNull(range2);
+
+            Assert.IsNotNull(range3);
+            Assert.IsFalse(range3.IsReverse);
+            Assert.AreEqual("2022-10-01", range3.Days[0].ToString());
+            Assert.AreEqual("2022-12-31", range3.Days[range3.Days.Length - 1].ToString());
+            Assert.AreEqual("2022-10-01__2022-12-31", range3.ToString());
+
+            Assert.IsNotNull(range4);
+            Assert.IsTrue(range4.IsReverse);
+            Assert.AreEqual("2022-12-31", range4.Days[0].ToString());
+            Assert.AreEqual("2022-10-01", range4.Days[range4.Days.Length - 1].ToString());
+            Assert.AreEqual("2022-12-31__2022-10-01", range4.ToString());
+
+        }
+
+    }
+
+}
