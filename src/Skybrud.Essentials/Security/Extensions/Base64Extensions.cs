ffd61ca9d222b9042bb806888dbcdda486251c0a
Anders Bjerner <abjerner@skybrud.dk>
2023-01-10 23:37:18 +0100

    Added extension methods for encoding and hasing string values

---
+++ src/Skybrud.Essentials/Security/Extensions/Base64Extensions.cs
@@ -0,0 +1,32 @@
+﻿#if I_CAN_HAZ_SECURITY
+
+namespace Skybrud.Essentials.Security.Extensions {
+
+    /// <summary>
+    /// Static class with variois Base64 related extension methods.
+    /// </summary>
+    public static class Base64Extensions {
+
+        /// <summary>
+        /// Returns a Base64 encoded version of the specified <paramref name="input"/> string.
+        /// </summary>
+        /// <param name="input">The input string to be encoded.</param>
+        /// <returns>The Base64 encoded string.</returns>
+        public static string Base64Encode(this string input) {
+            return SecurityUtils.Base64Encode(input);
+        }
+
+        /// <summary>
+        /// Returns a Base64 decoded version of the specified <paramref name="input"/> string.
+        /// </summary>
+        /// <param name="input">The input string to be decoded.</param>
+        /// <returns>The Base64 decoded string.</returns>
+        public static string Base64Decode(this string input) {
+            return SecurityUtils.Base64Decode(input);
+        }
+
+    }
+
+}
+
+#endif
---
+++ src/Skybrud.Essentials/Security/Extensions/HashingExtensions.cs
@@ -0,0 +1,376 @@
+﻿#if I_CAN_HAZ_SECURITY
+
+using System.Text;
+using System;
+using Skybrud.Essentials.Strings;
+
+namespace Skybrud.Essentials.Security.Extensions {
+
+    /// <summary>
+    /// Static class with various extension methods related to hashing.
+    /// </summary>
+    public static class HashingExtensions {
+
+        #region ToMd5Guid
+
+        /// <summary>
+        /// Returns the MD5 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static Guid ToMd5Guid(this string input) {
+            return SecurityUtils.GetMd5Guid(input);
+        }
+
+        /// <summary>
+        /// Gets the MD5 hash of <paramref name="input"/>, and returns that hash. The input string is converted using
+        /// <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static Guid ToMd5Guid(this string input, Encoding encoding) {
+            return SecurityUtils.GetMd5Guid(input, encoding);
+        }
+
+        #endregion
+
+        #region ToMd5Hash...
+
+        /// <summary>
+        /// Returns the MD5 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static string ToMd5Hash(this string input) {
+            return SecurityUtils.GetMd5Hash(input);
+        }
+
+        /// <summary>
+        /// Returns the MD5 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static string ToMd5Hash(this string input, HexFormat format) {
+            return SecurityUtils.GetMd5Hash(input, format);
+        }
+
+        /// <summary>
+        /// Returns the MD5 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static string ToMd5Hash(this string input, Encoding encoding) {
+            return SecurityUtils.GetMd5Hash(input, encoding);
+        }
+
+        /// <summary>
+        /// Returns the MD5 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 32-character hexadecimal number.</returns>
+        public static string ToMd5Hash(this string input, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetMd5Hash(input, format, encoding);
+        }
+
+        #endregion
+
+        #region ToSha1Hash...
+
+        /// <summary>
+        /// Returns the SHA1 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToSha1Hash(this string input) {
+            return SecurityUtils.GetSha1Hash(input);
+        }
+
+        /// <summary>
+        /// Returns the SHA1 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToSha1Hash(this string input, HexFormat format) {
+            return SecurityUtils.GetSha1Hash(input, format);
+        }
+
+        /// <summary>
+        /// Returns the SHA1 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToSha1Hash(this string input, Encoding encoding) {
+            return SecurityUtils.GetSha1Hash(input, encoding);
+        }
+
+        /// <summary>
+        /// Returns the SHA1 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToSha1Hash(this string input, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetSha1Hash(input, format, encoding);
+        }
+
+        #endregion
+
+        #region ToSha256Hash...
+
+        /// <summary>
+        /// Returns the SHA256 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToSha256Hash(this string input) {
+            return SecurityUtils.GetSha256Hash(input);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToSha256Hash(this string input, HexFormat format) {
+            return SecurityUtils.GetSha256Hash(input, format);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToSha256Hash(this string input, Encoding encoding) {
+            return SecurityUtils.GetSha256Hash(input, encoding);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of <paramref name="input"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToSha256Hash(this string input, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetSha256Hash(input, format, encoding);
+        }
+
+        #endregion
+
+        #region ToSha512Hash...
+
+        /// <summary>
+        /// Returns the SHA512 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToSha512Hash(this string input) {
+            return SecurityUtils.GetSha512Hash(input);
+        }
+
+        /// <summary>
+        /// Returns the SHA512 hash of <paramref name="input"/>. The input string is converted using <see cref="Encoding.UTF8"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToSha512Hash(this string input, HexFormat format) {
+            return SecurityUtils.GetSha512Hash(input, format);
+        }
+
+        /// <summary>
+        /// Returns the SHA512 hash of <paramref name="input"/>. The input string is converted using
+        /// <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToSha512Hash(this string input, Encoding encoding) {
+            return SecurityUtils.GetSha512Hash(input, encoding);
+        }
+
+        /// <summary>
+        /// Returns the SHA512 hash of <paramref name="input"/>. The input string is converted using
+        /// <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToSha512Hash(this string input, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetSha512Hash(input, format, encoding);
+        }
+
+        #endregion
+
+        #region ToHmacSha1Hash
+
+        /// <summary>
+        /// Returns the HMACSHA1 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToHmacSha1Hash(this string value, string key) {
+            return SecurityUtils.GetHmacSha1Hash(key, value);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA1 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToHmacSha1Hash(this string value, string key, HexFormat format) {
+            return SecurityUtils.GetHmacSha1Hash(key, value, format);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA1 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToHmacSha1Hash(this string value, string key, Encoding encoding) {
+            return SecurityUtils.GetHmacSha1Hash(key, value, encoding);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA1 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 40-character hexadecimal number.</returns>
+        public static string ToHmacSha1Hash(this string value, string key, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetHmacSha1Hash(key, value, format, encoding);
+        }
+
+        #endregion
+
+        #region ToHmacSha256Hash
+
+        /// <summary>
+        /// Returns the HMACSHA256 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToHmacSha256Hash(this string value, string key) {
+            return SecurityUtils.GetHmacSha256Hash(key, value);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA256 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToHmacSha256Hash(this string value, string key, HexFormat format) {
+            return SecurityUtils.GetHmacSha256Hash(key, value, format);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA256 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToHmacSha256Hash(this string value, string key, Encoding encoding) {
+            return SecurityUtils.GetHmacSha256Hash(key, value, encoding);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA256 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 64-character hexadecimal number.</returns>
+        public static string ToHmacSha256Hash(this string value, string key, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetHmacSha256Hash(key, value, format, encoding);
+        }
+
+        #endregion
+
+        #region ToHmacSha512Hash
+
+        /// <summary>
+        /// Returns the HMACSHA512 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToHmacSha512Hash(this string value, string key) {
+            return SecurityUtils.GetHmacSha512Hash(key, value);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA512 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToHmacSha512Hash(this string value, string key, HexFormat format) {
+            return SecurityUtils.GetHmacSha512Hash(key, value, format);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA512 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToHmacSha512Hash(this string value, string key, Encoding encoding) {
+            return SecurityUtils.GetHmacSha512Hash(key, value, encoding);
+        }
+
+        /// <summary>
+        /// Returns the HMACSHA512 hash generated from the specified <paramref name="key"/> and
+        /// <paramref name="value"/>. The input string is converted using <paramref name="encoding"/>.
+        /// </summary>
+        /// <param name="key">The key to be used for the hashing.</param>
+        /// <param name="value">The input value to be hashed.</param>
+        /// <param name="format">The format to be used for the HEX string.</param>
+        /// <param name="encoding">The encoding to be used for the conversion.</param>
+        /// <returns>The hash as a 128-character hexadecimal number.</returns>
+        public static string ToHmacSha512Hash(this string value, string key, HexFormat format, Encoding encoding) {
+            return SecurityUtils.GetHmacSha512Hash(key, value, format, encoding);
+        }
+
+        #endregion
+
+    }
+
+}
+
+#endif
