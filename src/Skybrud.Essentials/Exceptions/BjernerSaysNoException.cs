d7e310b9d36d8147d8969742020e7fe70a71b57d
Anders Bjerner <abjerner@skybrud.dk>
2023-07-13 10:11:53 +0200

    Call them Easter eggs if you will...

---
+++ src/Skybrud.Essentials/Exceptions/BjernerSaysNoException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Exceptions {
+
+    /// <summary>
+    /// Created as an internal joke.
+    /// </summary>
+    public class BjernerSaysNoException : Exception {
+
+        /// <summary>
+        /// Initializes a new exception with a standard message.
+        /// </summary>
+        public BjernerSaysNoException() : base("Bjerner says NO!!!") { }
+
+        /// <summary>
+        /// Initializes a new exception with the specified <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The message of the exeption.</param>
+        public BjernerSaysNoException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new exception with the specified <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The message of the exeption.</param>
+        /// <param name="innerException">An optional inner exception.</param>
+        public BjernerSaysNoException(string message, Exception? innerException) : base(message, innerException) { }
+
+    }
+
+}
---
+++ src/Skybrud.Essentials/Exceptions/ComputerSaysNoException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Exceptions {
+
+    /// <summary>
+    /// Created as an internal joke.
+    /// </summary>
+    public class ComputerSaysNoException : Exception {
+
+        /// <summary>
+        /// Initializes a new exception with a standard message.
+        /// </summary>
+        public ComputerSaysNoException() : base("Computer says NO!!!") { }
+
+        /// <summary>
+        /// Initializes a new exception with the specified <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The message of the exeption.</param>
+        public ComputerSaysNoException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new exception with the specified <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message">The message of the exeption.</param>
+        /// <param name="innerException">An optional inner exception.</param>
+        public ComputerSaysNoException(string message, Exception? innerException) : base(message, innerException) { }
+
+    }
+
+}
