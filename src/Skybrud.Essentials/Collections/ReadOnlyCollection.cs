d8ab691ddad1086ceede9934370392202f4ff41d
Anders Bjerner <abjerner@skybrud.dk>
2024-02-05 21:39:21 +0100

    Introduced new "ReadOnlyCollection" and "ReadOnlyList" classes

---
+++ src/Skybrud.Essentials/Collections/ReadOnlyCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Skybrud.Essentials.Collections;
+
+/// <summary>
+/// Static class for working with <see cref="IReadOnlyCollection{T}"/>.
+/// </summary>
+public static class ReadOnlyCollection {
+
+    /// <summary>
+    /// Returns an empty instance of <see cref="IReadOnlyCollection{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The item type of the collection.</typeparam>
+    /// <returns>An instance of <see cref="IReadOnlyCollection{T}"/>.</returns>
+    public static IReadOnlyCollection<T> Empty<T>() {
+        return ArrayUtils.Empty<T>();
+    }
+
+}
---
+++ src/Skybrud.Essentials/Collections/ReadOnlyList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Skybrud.Essentials.Collections;
+
+/// <summary>
+/// Static class for working with <see cref="IReadOnlyList{T}"/>.
+/// </summary>
+public static class ReadOnlyList {
+
+    /// <summary>
+    /// Returns an empty instance of <see cref="IReadOnlyList{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The item type of the list.</typeparam>
+    /// <returns>An instance of <see cref="IReadOnlyList{T}"/>.</returns>
+    public static IReadOnlyList<T> Empty<T>() {
+        return ArrayUtils.Empty<T>();
+    }
+
+}
