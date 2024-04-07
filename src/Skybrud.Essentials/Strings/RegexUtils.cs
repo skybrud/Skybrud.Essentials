a5c5148fd7a31dad557721fafda7967704cc10ee
Anders Bjerner <abjerner@skybrud.dk>
2024-04-07 19:05:46 +0200

    Added additional "IsMatch" extension method overloads
    The new overloads support additional scenarios - e.g. with more out parameters.

--- src/Skybrud.Essentials/Strings/RegexUtils.cs
+++ src/Skybrud.Essentials/Strings/RegexUtils.cs
@@ -115,6 +115,29 @@ namespace Skybrud.Essentials.Strings {
         /// <param name="input">The string to search for a match.</param>
         /// <param name="pattern">The regular expression pattern to match.</param>
         /// <param name="result1">The value of the first group.</param>
+        /// <param name="result2">The value of the second group.</param>
+        /// <param name="result3">The value of the third group.</param>
+        /// <param name="result4">The value of the fourth group.</param>
+        /// <param name="result5">The value of the fifth group.</param>
+        /// <param name="result6">The value of the sixth group.</param>
+        /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
+        public static bool IsMatch(string? input, [RegexPattern] string pattern, out string result1, out string result2, out string result3, out string result4, out string result5, out string result6) {
+            Match match = Regex.Match(input ?? string.Empty, pattern);
+            result1 = match.Groups[1].Value;
+            result2 = match.Groups[2].Value;
+            result3 = match.Groups[3].Value;
+            result4 = match.Groups[4].Value;
+            result5 = match.Groups[5].Value;
+            result6 = match.Groups[6].Value;
+            return match.Success;
+        }
+
+        /// <summary>
+        /// Indicates whether the specified regular expression finds a match in the specified input string.
+        /// </summary>
+        /// <param name="input">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <param name="result1">The value of the first group.</param>
         /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
         public static bool IsMatch(string? input, [RegexPattern] string pattern, out int result1) {
             Match match = Regex.Match(input ?? string.Empty, pattern);
@@ -191,6 +214,29 @@ namespace Skybrud.Essentials.Strings {
             result3 = match.Groups[3].Value.ToInt32();
             result4 = match.Groups[4].Value.ToInt32();
             result5 = match.Groups[5].Value.ToInt32();
+            return match.Success;
+        }
+
+        /// <summary>
+        /// Indicates whether the specified regular expression finds a match in the specified input string.
+        /// </summary>
+        /// <param name="input">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <param name="result1">The value of the first group.</param>
+        /// <param name="result2">The value of the second group.</param>
+        /// <param name="result3">The value of the third group.</param>
+        /// <param name="result4">The value of the fourth group.</param>
+        /// <param name="result5">The value of the fifth group.</param>
+        /// <param name="result6">The value of the sixth group.</param>
+        /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
+        public static bool IsMatch(string? input, [RegexPattern] string pattern, out int result1, out int result2, out int result3, out int result4, out int result5, out int result6) {
+            Match match = Regex.Match(input ?? string.Empty, pattern);
+            result1 = match.Groups[1].Value.ToInt32();
+            result2 = match.Groups[2].Value.ToInt32();
+            result3 = match.Groups[3].Value.ToInt32();
+            result4 = match.Groups[4].Value.ToInt32();
+            result5 = match.Groups[5].Value.ToInt32();
+            result6 = match.Groups[6].Value.ToInt32();
             return match.Success;
         }
 
@@ -304,6 +350,30 @@ namespace Skybrud.Essentials.Strings {
         /// <param name="pattern">The regular expression pattern to match.</param>
         /// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
         /// <param name="result1">The value of the first group.</param>
+        /// <param name="result2">The value of the second group.</param>
+        /// <param name="result3">The value of the third group.</param>
+        /// <param name="result4">The value of the fourth group.</param>
+        /// <param name="result5">The value of the fifth group.</param>
+        /// <param name="result6">The value of the sixth group.</param>
+        /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
+        public static bool IsMatch(string? input, [RegexPattern] string pattern, RegexOptions options, out string result1, out string result2, out string result3, out string result4, out string result5, out string result6) {
+            Match match = Regex.Match(input ?? string.Empty, pattern, options);
+            result1 = match.Groups[1].Value;
+            result2 = match.Groups[2].Value;
+            result3 = match.Groups[3].Value;
+            result4 = match.Groups[4].Value;
+            result5 = match.Groups[5].Value;
+            result6 = match.Groups[6].Value;
+            return match.Success;
+        }
+
+        /// <summary>
+        /// Indicates whether the specified regular expression finds a match in the specified input string.
+        /// </summary>
+        /// <param name="input">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
+        /// <param name="result1">The value of the first group.</param>
         /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
         public static bool IsMatch(string? input, [RegexPattern] string pattern, RegexOptions options, out int result1) {
             Match match = Regex.Match(input ?? string.Empty, pattern, options);
@@ -384,6 +454,30 @@ namespace Skybrud.Essentials.Strings {
             result3 = match.Groups[3].Value.ToInt32();
             result4 = match.Groups[4].Value.ToInt32();
             result5 = match.Groups[5].Value.ToInt32();
+            return match.Success;
+        }
+
+        /// <summary>
+        /// Indicates whether the specified regular expression finds a match in the specified input string.
+        /// </summary>
+        /// <param name="input">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <param name="options">A bitwise combination of the enumeration values that provide options for matching.</param>
+        /// <param name="result1">The value of the first group.</param>
+        /// <param name="result2">The value of the second group.</param>
+        /// <param name="result3">The value of the third group.</param>
+        /// <param name="result4">The value of the fourth group.</param>
+        /// <param name="result5">The value of the fifth group.</param>
+        /// <param name="result6">The value of the sixth group.</param>
+        /// <returns><c>true</c> if the regular expression finds a match; otherwise, <c>false</c>.</returns>
+        public static bool IsMatch(string? input, [RegexPattern] string pattern, RegexOptions options, out int result1, out int result2, out int result3, out int result4, out int result5, out int result6) {
+            Match match = Regex.Match(input ?? string.Empty, pattern, options);
+            result1 = match.Groups[1].Value.ToInt32();
+            result2 = match.Groups[2].Value.ToInt32();
+            result3 = match.Groups[3].Value.ToInt32();
+            result4 = match.Groups[4].Value.ToInt32();
+            result5 = match.Groups[5].Value.ToInt32();
+            result6 = match.Groups[6].Value.ToInt32();
             return match.Success;
         }
 
