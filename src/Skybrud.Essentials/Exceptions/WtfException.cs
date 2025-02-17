c42d16d22b61fbf53b0a2592a3591c717456a596
Anders Bjerner <abjerner@skybrud.dk>
2025-02-18 00:09:12 +0100

    Might be useful someday...

---
+++ src/Skybrud.Essentials/Exceptions/WtfException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Exceptions;
+
+/// <summary>
+/// Exception class for those truly WTF moments...
+/// </summary>
+public class WtfException : Exception {
+
+    /// <summary>
+    /// Initializes a new exception with a standard message.
+    /// </summary>
+    public WtfException() : base("WTF?!?") { }
+
+    /// <summary>
+    /// Initializes a new exception with the specified <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">The message of the exeption.</param>
+    public WtfException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new exception with the specified <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">The message of the exeption.</param>
+    /// <param name="innerException">An optional inner exception.</param>
+    public WtfException(string message, Exception? innerException) : base(message, innerException) { }
+
+}
