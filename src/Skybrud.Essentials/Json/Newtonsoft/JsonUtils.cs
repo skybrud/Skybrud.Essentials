7b7434f86bda58ff9c8eebdcaf8811092b9a5209
Anders Bjerner <abj@limbo.works>
2025-06-10 11:36:16 +0200

    Introduced new "JsonUtils.LoadJsonToken", "JsonUtils.LoadJsonObject", and "JsonUtils.LoadJsonArray" method overloads
    The existing methods supports loading JSON values from a path the disk, whereas the new methods allow loading JSON values from a stream.

--- src/Skybrud.Essentials/Json/Newtonsoft/JsonUtils.cs
+++ src/Skybrud.Essentials/Json/Newtonsoft/JsonUtils.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Collections.Extensions;
 using Skybrud.Essentials.Common;
 
 namespace Skybrud.Essentials.Json.Newtonsoft;
@@ -397,6 +398,146 @@ public static class JsonUtils {
 
     }
 
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An instance of <see cref="JToken"/>.</returns>
+    public static JToken LoadJsonToken(Stream stream) {
+        using StreamReader reader = new(stream);
+        using JsonTextReader jsonTextReader = new(reader);
+        jsonTextReader.DateParseHandling = DateParseHandling.None;
+        return JToken.Load(jsonTextReader);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <param name="objectType">The type the JSON token should be converted to.</param>
+    /// <returns>An instance of <paramref name="objectType"/>.</returns>
+    public static object LoadJsonToken(Stream stream, Type objectType) {
+        return LoadJsonToken(stream).ToObject(objectType);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type to be returned.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/>.</returns>
+    public static TResult LoadJsonToken<TResult>(Stream stream) {
+        return LoadJsonToken(stream).ToObject<TResult>();
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type to which the JSON token should be converted.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <param name="func">A callback function used for converting the JSON token.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/>.</returns>
+    public static TResult LoadJsonToken<TResult>(Stream stream, Func<JToken, TResult> func) {
+        return func(LoadJsonToken(stream));
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON object from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An instance of <see cref="JObject"/>.</returns>
+    public static JObject LoadJsonObject(Stream stream) {
+        using StreamReader reader = new(stream);
+        using JsonTextReader jsonTextReader = new(reader);
+        jsonTextReader.DateParseHandling = DateParseHandling.None;
+        return JObject.Load(jsonTextReader);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON object from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <param name="objectType">The type the JSON object should be converted to.</param>
+    /// <returns>An instance of <paramref name="objectType"/>.</returns>
+    public static object LoadJsonObject(Stream stream, Type objectType) {
+        return LoadJsonObject(stream).ToObject(objectType);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON object from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type to be returned.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/>.</returns>
+    public static TResult LoadJsonObject<TResult>(Stream stream) {
+        return LoadJsonObject(stream).ToObject<TResult>();
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON object from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type to which the JSON object should be converted.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <param name="func">A callback function used for converting the JSON object.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/>.</returns>
+    public static TResult LoadJsonObject<TResult>(Stream stream, Func<JObject, TResult> func) {
+        return func(LoadJsonObject(stream));
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON array from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An instance of <see cref="JToken"/>.</returns>
+    public static JArray LoadJsonArray(Stream stream) {
+        using StreamReader reader = new(stream);
+        using JsonTextReader jsonTextReader = new(reader);
+        jsonTextReader.DateParseHandling = DateParseHandling.None;
+        return JArray.Load(jsonTextReader);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream.</param>
+    /// <param name="objectType">The type of the array.</param>
+    /// <returns>An array of <paramref name="objectType"/>.</returns>
+    public static object LoadJsonArray(Stream stream, Type objectType) {
+        return LoadJsonArray(stream).ToObject(objectType);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the items in the array.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <returns>An array of <typeparamref name="TItem"/>.</returns>
+    public static TItem[] LoadJsonArray<TItem>(Stream stream) {
+        return LoadJsonArray(stream).ToObject<TItem[]>();
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the items in the array.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <param name="func">A callback function for converting each item to an instance of <typeparamref name="TItem"/>.</param>
+    /// <returns>An array of <typeparamref name="TItem"/>.</returns>
+    public static TItem[] LoadJsonArray<TItem>(Stream stream, Func<JObject, TItem> func) {
+        return LoadJsonArray(stream).OfType<JObject>().Select(func).ToArray();
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type returned by the conversion.</typeparam>
+    /// <param name="stream">The stream.</param>
+    /// <param name="func">A callback function used for converting the JSON array.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/> representing the array.</returns>
+    public static TResult LoadJsonArray<TResult>(Stream stream, Func<JArray, TResult> func) {
+        return func(LoadJsonArray(stream));
+    }
+
 #if I_CAN_HAZ_FILE
 
     /// <summary>
@@ -406,6 +547,16 @@ public static class JsonUtils {
     /// <returns>An instance of <see cref="JObject"/>.</returns>
     public static JToken LoadJsonToken(string path) {
         return ParseJsonToken(File.ReadAllText(path, Encoding.UTF8));
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON token in the file at the specified <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <param name="objectType">The type the JSON token should be converted to.</param>
+    /// <returns>An instance of <paramref name="objectType"/>.</returns>
+    public static object LoadJsonToken(string path, Type objectType) {
+        return LoadJsonToken(path).ToObject(objectType);
     }
 
     /// <summary>
@@ -478,6 +629,26 @@ public static class JsonUtils {
     }
 
     /// <summary>
+    /// Loads and parses the JSON array at the specified <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <param name="objectType">The type of the items in the array.</param>
+    /// <returns>An array of <paramref name="objectType"/>.</returns>
+    public static Array LoadJsonArray(string path, Type objectType) {
+        return LoadJsonArray(path).Select(x => x.ToObject(objectType)).ToArray(objectType);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON array at the specified <paramref name="path"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the items in the array.</typeparam>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <returns>An array of <typeparamref name="TItem"/>.</returns>
+    public static TItem[] LoadJsonArray<TItem>(string path) {
+        return LoadJsonArray(path).ToObject<TItem[]>();
+    }
+
+    /// <summary>
     /// Loads and parses the JSON object in the file at the specified <paramref name="path"/>.
     /// </summary>
     /// <typeparam name="T">The type to be returned.</typeparam>
@@ -487,6 +658,17 @@ public static class JsonUtils {
     /// <returns>An instance of <typeparamref name="T"/>.</returns>
     public static T[] LoadJsonArray<T>(string path, Func<JObject, T> func) {
         return ParseJsonArray(File.ReadAllText(path, Encoding.UTF8), func);
+    }
+
+    /// <summary>
+    /// Loads and parses the JSON object in the file at the specified <paramref name="path"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type returned by the conversion.</typeparam>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <param name="func">A callback function used for converting the JSON array.</param>
+    /// <returns>An instance of <typeparamref name="TResult"/> representing the array.</returns>
+    public static TResult LoadJsonArray<TResult>(string path, Func<JArray, TResult> func) {
+        return func(LoadJsonArray(path));
     }
 
     /// <summary>
---
+++ src/TestProject1/Json/Newtonsoft/LoadStreamTests.cs
@@ -0,0 +1,270 @@
+﻿using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Exceptions;
+using Skybrud.Essentials.IO;
+using Skybrud.Essentials.Json.Newtonsoft;
+using Skybrud.Essentials.Json.Newtonsoft.Extensions;
+
+namespace TestProject1.Json.Newtonsoft;
+
+[TestClass]
+public class LoadStreamTests {
+
+    protected const string ObjectSample = """
+                                          {
+                                              "hello": "world"
+                                          }
+                                          """;
+
+    protected const string ArraySample = """
+                                         [
+                                             {
+                                                 "hello": "world"
+                                             }
+                                         ]
+                                         """;
+    [TestMethod]
+    public void LoadJsonToken1() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        JObject? result = JsonUtils.LoadJsonToken(stream) as JObject;
+
+        Assert.IsNotNull(result);
+
+        string? hello = result.GetString("hello");
+
+        Assert.AreEqual("world", hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonToken2() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass? result = JsonUtils.LoadJsonToken(stream, typeof(SampleClass)) as SampleClass;
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonToken3() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass result = JsonUtils.LoadJsonToken<SampleClass>(stream);
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonToken4() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass result = JsonUtils.LoadJsonToken(stream, SampleClass.ParseToken);
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+
+
+
+
+
+    [TestMethod]
+    public void LoadJsonObject1() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        JObject result = JsonUtils.LoadJsonObject(stream);
+
+        Assert.IsNotNull(result);
+
+        string? hello = result.GetString("hello");
+
+        Assert.AreEqual("world", hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonObject2() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass? result = JsonUtils.LoadJsonObject(stream, typeof(SampleClass)) as SampleClass;
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonObject3() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass result = JsonUtils.LoadJsonObject<SampleClass>(stream);
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonObject4() {
+
+        using Stream stream = StreamUtils.ToStream(ObjectSample);
+
+        SampleClass result = JsonUtils.LoadJsonObject(stream, SampleClass.ParseObject);
+
+        Assert.IsNotNull(result);
+
+        Assert.AreEqual("world", result.Hello);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    [TestMethod]
+    public void LoadJsonArray1() {
+
+        using Stream stream = StreamUtils.ToStream(ArraySample);
+
+        JArray result = JsonUtils.LoadJsonArray(stream);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count);
+
+        JObject? first = result.FirstOrDefault() as JObject;
+
+        string? hello = first.GetString("hello");
+
+        Assert.AreEqual("world", hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonArray2() {
+
+        using Stream stream = StreamUtils.ToStream(ArraySample);
+
+        SampleClass[]? result = JsonUtils.LoadJsonArray(stream, typeof(SampleClass[])) as SampleClass[];
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Length);
+
+        Assert.AreEqual("world", result[0].Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonArray3() {
+
+        using Stream stream = StreamUtils.ToStream(ArraySample);
+
+        SampleClass[] result = JsonUtils.LoadJsonArray<SampleClass>(stream);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Length);
+
+        Assert.AreEqual("world", result[0].Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonArray4() {
+
+        using Stream stream = StreamUtils.ToStream(ArraySample);
+
+        SampleClass[] result = JsonUtils.LoadJsonArray(stream, SampleClass.ParseObject);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Length);
+
+        Assert.AreEqual("world", result[0].Hello);
+
+    }
+
+    [TestMethod]
+    public void LoadJsonArray5() {
+
+        using Stream stream = StreamUtils.ToStream(ArraySample);
+
+        SampleList result = JsonUtils.LoadJsonArray(stream, SampleList.Parse);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count);
+
+        Assert.AreEqual("world", result[0].Hello);
+
+    }
+
+
+
+    public class SampleList : List<SampleClass> {
+
+        public static SampleList Parse(JArray array) {
+
+            SampleList list = [];
+
+            foreach (JToken token in array) {
+
+                if (token is not JObject obj) throw new ComputerSaysNoException("Not an instance of 'JObject'.");
+
+                list.Add(SampleClass.ParseObject(obj));
+
+            }
+
+            return list;
+
+        }
+
+    }
+
+    public class SampleClass {
+
+        public string? Hello { get; set; }
+
+        public SampleClass() { }
+
+        public SampleClass(JObject json) {
+            Hello = json.GetString("hello");
+        }
+
+        public static SampleClass ParseObject(JObject json) {
+            return new SampleClass(json);
+        }
+
+        public static SampleClass ParseToken(JToken token) {
+            return new SampleClass(token as JObject ?? throw new ComputerSaysNoException());
+        }
+
+    }
+
+}
