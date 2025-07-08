caf6825f0ed961283a1cdb64a5827f25049386c1
Anders Bjerner <abj@limbo.works>
2025-07-08 21:17:17 +0200

    Introduced new "CamelCaseJsonContractResolver" class for JSON.net

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/Serialization/CamelCaseJsonContractResolver.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace Skybrud.Essentials.Json.Newtonsoft.Serialization;
+
+/// <summary>
+/// Contract resolver using a <see cref="CamelCaseNamingStrategy"/>.
+/// </summary>
+public class CamelCaseJsonContractResolver : DefaultContractResolver {
+
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    public CamelCaseJsonContractResolver() {
+        NamingStrategy = new CamelCaseNamingStrategy();
+    }
+
+}
