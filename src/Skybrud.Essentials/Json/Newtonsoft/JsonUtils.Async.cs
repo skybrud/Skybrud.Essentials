2d68d29e8691a525a04b6f47f83d0972c15ff0bd
Anders Bjerner <abj@limbo.works>
2025-07-08 21:23:31 +0200

    Added misc async methods to the "JsonUtils" class
    More methods in the class should probably have an async counterpart, but this commit should as least serve as a start towards that.

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/JsonUtils.Async.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Skybrud.Essentials.Json.Newtonsoft;
+
+public partial class JsonUtils {
+
+#if NET5_0_OR_GREATER
+
+    /// <summary>
+    /// Loads and parses the JSON object from the file at the specified <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <returns></returns>
+    public static async Task<JObject> LoadJsonObjectAsync(string path) {
+        return ParseJsonObject(await File.ReadAllTextAsync(path, Encoding.UTF8));
+    }
+
+    /// <summary>
+    /// Saves the specified <paramref name="json" /> object to a file at <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <param name="json">The JSON object to be saved</param>
+    public static async Task SaveJsonObjectAsync(string path, JObject json) {
+        await SaveJsonObjectAsync(path, json, Formatting.None);
+    }
+
+    /// <summary>
+    /// Saves the specified <paramref name="json" /> object to a file at <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to the JSON file.</param>
+    /// <param name="json">The JSON object to be saved</param>
+    /// <param name="formatting">The formatting to be used when saving the JSON object.</param>
+    public static async Task SaveJsonObjectAsync(string path, JObject json, Formatting formatting) {
+        if (string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException(nameof(path));
+        if (json == null) throw new ArgumentNullException(nameof(json));
+        await File.WriteAllTextAsync(path, json.ToString(formatting), Encoding.UTF8);
+    }
+
+#endif
+
+}
