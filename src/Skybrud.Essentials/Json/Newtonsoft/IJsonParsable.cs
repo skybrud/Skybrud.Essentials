5bda20c49cfd8f888681b3313e0e6dbbbe911d11
Anders Bjerner <abj@limbo.works>
2025-09-21 18:45:22 +0200

    Introduced new "IJsonParsable<TSelf>" interface
    This interface can be used to indicate that a type can be parsed from a "JObject" instance.

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/IJsonParsable.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Skybrud.Essentials.Json.Newtonsoft;
+
+/// <summary>
+/// Interface describing a class that may be parsed from an instance of <see cref="JObject"/>.
+/// </summary>
+/// <typeparam name="TSelf">The type of the class.</typeparam>
+public interface IJsonParsable<out TSelf> {
+
+#if NET8_0_OR_GREATER
+
+    /// <summary>
+    /// Parses the specified <paramref name="json"/> object into an instance of <typeparamref name="TSelf"/>.
+    /// </summary>
+    /// <param name="json">The JSON object to parse.</param>
+    /// <returns>An instance of <typeparamref name="TSelf"/>.</returns>
+    static abstract TSelf Parse(JObject json);
+
+#endif
+
+}
