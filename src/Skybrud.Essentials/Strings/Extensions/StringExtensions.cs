a01b5f1330dd99c3f3fd857b9ecf194d696add26
Anders Bjerner <abj@limbo.works>
2025-10-16 14:19:31 +0200

    Fixed issue with "ToInvariantString" extension methods
    For the overloading taking an "IFormattable", we shouldn't use "string.format" as the format is directly for the "IFormattable".

--- src/Skybrud.Essentials/Strings/Extensions/StringExtensions.cs
+++ src/Skybrud.Essentials/Strings/Extensions/StringExtensions.cs
@@ -114,7 +114,7 @@ public static partial class StringExtensions {
     /// </summary>
     /// <param name="input">The input string to test.</param>
     /// <returns><c>true</c> if <paramref name="input"/> has a value; otherwise <c>false</c>.</returns>
-    public static bool HasValue(this string? input) {
+    public static bool HasValue([NotNullWhen(true)] this string? input) {
         return string.IsNullOrWhiteSpace(input) == false;
     }
 
@@ -126,7 +126,7 @@ public static partial class StringExtensions {
     /// <param name="input">The input string to test.</param>
     /// <param name="result">When this method returns, contains the input value.</param>
     /// <returns><c>true</c> if <paramref name="input"/> has a value; otherwise <c>false</c>.</returns>
-    public static bool HasValue(this string? input, [NotNullWhen(true)] out string? result) {
+    public static bool HasValue([NotNullWhen(true)] this string? input, [NotNullWhen(true)] out string? result) {
         result = input;
         return string.IsNullOrWhiteSpace(input) == false;
     }
@@ -137,7 +137,7 @@ public static partial class StringExtensions {
     /// </summary>
     /// <param name="input">The input string to test.</param>
     /// <returns><c>true</c> if <paramref name="input"/> has a value; otherwise <c>false</c>.</returns>
-    public static bool IsNullOrWhiteSpace(this string? input) {
+    public static bool IsNullOrWhiteSpace([NotNullWhen(true)] this string? input) {
         return string.IsNullOrWhiteSpace(input);
     }
 
@@ -146,7 +146,7 @@ public static partial class StringExtensions {
     /// </summary>
     /// <param name="input">The string to validate.</param>
     /// <returns><c>true</c> if <paramref name="input"/> matches a double; otherwise <c>false</c>.</returns>
-    public static bool IsNumeric(this string? input) {
+    public static bool IsNumeric([NotNullWhen(true)] this string? input) {
         return StringUtils.IsNumeric(input);
     }
 
@@ -155,7 +155,7 @@ public static partial class StringExtensions {
     /// </summary>
     /// <param name="input">The string to validate.</param>
     /// <returns><c>true</c> if <paramref name="input"/> is alphanumeric; otherwise <c>false</c>.</returns>
-    public static bool IsAlphanumeric(this string? input) {
+    public static bool IsAlphanumeric([NotNullWhen(true)] this string? input) {
         return StringUtils.IsAlphanumeric(input);
     }
 
@@ -164,7 +164,7 @@ public static partial class StringExtensions {
     /// </summary>
     /// <param name="input">The string to validate.</param>
     /// <returns><c>true</c> if <paramref name="input"/> is alphanumeric; otherwise <c>false</c>.</returns>
-    public static bool IsAlphabetic(this string? input) {
+    public static bool IsAlphabetic([NotNullWhen(true)] this string? input) {
         return StringUtils.IsAlphabetic(input);
     }
 
@@ -830,7 +830,7 @@ public static partial class StringExtensions {
     /// <returns>A culture invariant string representation of <paramref name="value"/>.</returns>
     [return: NotNullIfNotNull(nameof(value))]
     public static string? ToInvariantString(this IFormattable? value, string format) {
-        return value is null ? null : string.Format(CultureInfo.InvariantCulture, format, value);
+        return value?.ToString(format, CultureInfo.InvariantCulture);
     }
 
     /// <summary>
@@ -841,7 +841,7 @@ public static partial class StringExtensions {
     /// <returns>A culture invariant string representation of <paramref name="value"/>.</returns>
     [return: NotNullIfNotNull(nameof(value))]
     public static string? ToInvariantString(this DateTime? value, [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format) {
-        return value is null ? null : string.Format(CultureInfo.InvariantCulture, format, value);
+        return value?.ToInvariantString(format);
     }
 
     /// <summary>
@@ -852,7 +852,7 @@ public static partial class StringExtensions {
     /// <returns>A culture invariant string representation of <paramref name="value"/>.</returns>
     [return: NotNullIfNotNull(nameof(value))]
     public static string? ToInvariantString(this DateTimeOffset? value, [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format) {
-        return value is null ? null : string.Format(CultureInfo.InvariantCulture, format, value);
+        return value?.ToInvariantString(format);
     }
 
     /// <summary>
@@ -863,7 +863,7 @@ public static partial class StringExtensions {
     /// <returns>A culture invariant string representation of <paramref name="value"/>.</returns>
     [return: NotNullIfNotNull(nameof(value))]
     public static string? ToInvariantString(this TimeSpan? value, [StringSyntax(StringSyntaxAttribute.TimeSpanFormat)] string format) {
-        return value is null ? null : string.Format(CultureInfo.InvariantCulture, format, value);
+        return value?.ToInvariantString(format);
     }
 
 }
