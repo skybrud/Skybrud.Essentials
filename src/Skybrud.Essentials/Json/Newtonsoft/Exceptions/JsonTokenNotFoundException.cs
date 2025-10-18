465b5c661cf9c659d9ebc050282429c7488b7b04
Anders Bjerner <abj@limbo.works>
2025-10-18 13:29:41 +0200

    Introduced new "JsonTokenNotFoundException" exception class
    The exception can be thrown when a required token isn't found.

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/Exceptions/JsonTokenNotFoundException.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Skybrud.Essentials.Json.Newtonsoft.Exceptions;
+
+/// <summary>
+/// Exception thrown when a required JSON token was not found.
+/// </summary>
+public class JsonTokenNotFoundException : JsonException {
+
+    /// <summary>
+    /// Gets a reference to the JSON object.
+    /// </summary>
+    public new JObject Source { get; }
+
+    /// <summary>
+    /// Gets the path of the JSON token.
+    /// </summary>
+    public string Path { get; }
+
+    /// <summary>
+    /// Initializes a new exception based on the specified <paramref name="source"/> and <paramref name="path"/>.
+    /// </summary>
+    /// <param name="source">A reference to the JSON object where the token was expected.</param>
+    /// <param name="path">The path to the JSON token.</param>
+    public JsonTokenNotFoundException(JObject source, string path) : base($"A required token with path '{path}' could not be not found.") {
+        Source = source;
+        Path = path;
+    }
+
+    /// <summary>
+    /// Initializes a new exception based on the specified <paramref name="source"/> and <paramref name="path"/>.
+    /// </summary>
+    /// <param name="source">A reference to the JSON object where the token was expected.</param>
+    /// <param name="path">The path to the JSON token.</param>
+    /// <param name="message">The exception message.</param>
+    public JsonTokenNotFoundException(JObject source, string path, string message) : base(message) {
+        Source = source;
+        Path = path;
+    }
+
+}
