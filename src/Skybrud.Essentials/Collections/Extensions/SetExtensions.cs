8d73338cf7acfe66c7fa12356deffe9b1fdab1df
Anders Bjerner <abj@limbo.works>
2026-03-05 00:04:59 +0100

    Bah

--- src/Skybrud.Essentials/Collections/Extensions/SetExtensions.cs
+++ src/Skybrud.Essentials/Collections/Extensions/SetExtensions.cs
@@ -11,7 +11,7 @@ public static class SetExtensions {
     /// Adds the elements of the specified collection to the end of the <see cref="ISet{T}"/>.
     /// </summary>
     /// <typeparam name="T">The element type.</typeparam>
-    /// <param name="set">The set to which the elements of <see cref="collection"/> should be added.</param>
+    /// <param name="set">The set to which the elements of <paramref name="collection"/> should be added.</param>
     /// <param name="collection">The collection whose elements should be added to the end of the <see cref="ISet{T}"/>. The collection itself cannot be null, but it can contain elements that are null, if type <typeparamref name="T"/> is a reference type.</param>
     public static void AddRange<T>(this ISet<T> set, IEnumerable<T> collection) {
         foreach (T item in collection) set.Add(item);
