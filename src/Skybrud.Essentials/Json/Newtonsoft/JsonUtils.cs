754140fcf76d1af6592fdcff15c7b3057736c12f
Anders Bjerner <abj@limbo.works>
2026-06-20 14:01:07 +0200

    Yes please!

--- src/Skybrud.Essentials/Json/Newtonsoft/JsonUtils.cs
+++ src/Skybrud.Essentials/Json/Newtonsoft/JsonUtils.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Skybrud.Essentials.Collections.Extensions;
+using Skybrud.Essentials.Collections.Enumerables.Extensions;
 using Skybrud.Essentials.Common;
 
 namespace Skybrud.Essentials.Json.Newtonsoft;
