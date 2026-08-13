3d99e4463c368f07b69e704e405a45686da89840
Anders Bjerner <abj@limbo.works>
2026-08-13 14:57:47 +0200

    Added "TimeSpanSecondsConverter" and  "TimeSpanMillisecondsConverter" converters for Newtonsoft.Json
    Copied from the v1.1 branch (see 197f4f7f4be07b23420a3c7a95fdcf843be4b252)

---
+++ src/Skybrud.Essentials/Json/Newtonsoft/Converters/Time/TimeSpanSecondsConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Skybrud.Essentials.Time;
+
+namespace Skybrud.Essentials.Json.Newtonsoft.Converters.Time;
+
+/// <summary>
+/// JSON converter for serializing and deserializing between instances of <see cref="TimeSpan"/> and the total
+/// amount of seconds represented by the time span.
+/// </summary>
+public class TimeSpanSecondsConverter : TimeSpanConverter {
+
+    /// <summary>
+    /// Initialized a new instance.
+    /// </summary>
+    public TimeSpanSecondsConverter() : base(TimeSpanFormat.Seconds)  { }
+
+}
