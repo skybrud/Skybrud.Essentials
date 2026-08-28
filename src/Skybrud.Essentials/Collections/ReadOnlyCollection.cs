8c1c1dbb3504d5d61e7f173372f26bd57f0635e4
Anders Bjerner <abj@limbo.works>
2026-08-28 14:11:53 +0200

    Added misc non-generic collection initializers
    Backported from the v1.2 package

--- src/Skybrud.Essentials/Collections/ReadOnlyCollection.cs
+++ src/Skybrud.Essentials/Collections/ReadOnlyCollection.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Skybrud.Essentials.Collections;
 
@@ -6,6 +10,20 @@ namespace Skybrud.Essentials.Collections;
 /// Static class for working with <see cref="IReadOnlyCollection{T}"/>.
 /// </summary>
 public static class ReadOnlyCollection {
+
+    /// <summary>
+    /// Returns an empty read-only collection of the specified <paramref name="itemType"/>.
+    /// </summary>
+    /// <param name="itemType">The type of the items.</param>
+    /// <returns>A read-only collection of <paramref name="itemType"/>.</returns>
+    public static IEnumerable Empty(Type itemType) {
+        return (IEnumerable) typeof(ReadOnlyCollection)
+            .GetTypeInfo()
+            .GetDeclaredMethods(nameof(Empty))
+            .First(x => x.GetParameters().Length == 0)
+            .MakeGenericMethod(itemType)
+            .Invoke(null, null)!;
+    }
 
     /// <summary>
     /// Returns an empty instance of <see cref="IReadOnlyCollection{T}"/>.
--- src/Skybrud.Essentials/Collections/ReadOnlyList.cs
+++ src/Skybrud.Essentials/Collections/ReadOnlyList.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Skybrud.Essentials.Collections;
 
@@ -6,6 +10,20 @@ namespace Skybrud.Essentials.Collections;
 /// Static class for working with <see cref="IReadOnlyList{T}"/>.
 /// </summary>
 public static class ReadOnlyList {
+
+    /// <summary>
+    /// Returns an empty read-only list of the specified <paramref name="itemType"/>.
+    /// </summary>
+    /// <param name="itemType">The type of the items.</param>
+    /// <returns>A read-only list of <paramref name="itemType"/>.</returns>
+    public static IEnumerable Empty(Type itemType) {
+        return (IEnumerable) typeof(ReadOnlyList)
+            .GetTypeInfo()
+            .GetDeclaredMethods(nameof(Empty))
+            .First(x => x.GetParameters().Length == 0)
+            .MakeGenericMethod(itemType)
+            .Invoke(null, null)!;
+    }
 
     /// <summary>
     /// Returns an empty instance of <see cref="IReadOnlyList{T}"/>.
